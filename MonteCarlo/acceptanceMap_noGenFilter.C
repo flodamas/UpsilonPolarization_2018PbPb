58511d5dd56ee14eeff0d7fe3cda1f4b8e8596a4
Saeahram <85007865+ashleyahram@users.noreply.github.com>
2023-07-05 16:56:32 +0200

    Add the acceptance map in the lab frame
    Add the acceptance map in the lab frame (It was for the presentation)

--- MonteCarlo/acceptanceMap_noGenFilter.C
+++ MonteCarlo/acceptanceMap_noGenFilter.C
@@ -24,6 +24,8 @@
 // MC file available here: /eos/cms/store/group/phys_heavyions/dileptons/MC2015/pp502TeV/TTrees/OniaTree_Ups1SMM_5p02TeV_TuneCUETP8M1_nofilter_pp502Fall15-MCRUN2_71_V1-v1_GENONLY.root
 
 void acceptanceMap_noGenFilter(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 30) {
+
+	// Read GenOnly Nofilter file
 	const char* filename = Form("../Files/OniaTree_Y%dS_GENONLY_NoFilter.root", iState);
 	TFile* file = TFile::Open(filename, "READ");
 	if (!file) {
@@ -33,12 +35,14 @@ void acceptanceMap_noGenFilter(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 
 
 	cout << "File " << filename << " opened" << endl;
 
-	writeExtraText = true; // if extra text
-	extraText = "       Internal";
+	// Put the text, "CMS Internal", on the right top of the plot 
+	// writeExtraText = true; // if extra text
+	// extraText = "       Internal";
+	writeExtraText = false;
 
 	TTree* OniaTree = (TTree*)file->Get("hionia/myTree");
 
-	/// OniaTree variables, quite old version
+	/// OniaTree variables, quite old version (since this genonly file from 2015)
 	Int_t Gen_QQ_size;
 
 	TClonesArray* Gen_QQ_4mom = nullptr;
@@ -51,8 +55,7 @@ void acceptanceMap_noGenFilter(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 
 	OniaTree->SetBranchAddress("Gen_QQ_mumi_4mom", &Gen_QQ_mumi_4mom);
 	OniaTree->SetBranchAddress("Gen_QQ_mupl_4mom", &Gen_QQ_mupl_4mom);
 
-	// (cos theta, phi) 2D distribution maps for CS and HX frames
-
+	// (cos theta, phi) 2D distribution maps for Lab, CS and HX frames
 	Int_t nCosThetaBins = 20;
 	Float_t cosThetaMin = -1, cosThetaMax = 1;
 
@@ -65,6 +68,8 @@ void acceptanceMap_noGenFilter(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 
 	//Double_t phiBinning[] = {-TMath::Pi(), -2.5, -2, -1.5, -1, -0.5, 0.5, 1, 1.5, 2, 2.5, 180};
 	//	nPhiBins = sizeof(cosThetaBinning) / sizeof(Double_t) - 1;
 
+
+	TEfficiency* hLab = new TEfficiency(Form("Lab_pt%dto%dGeV", ptMin, ptMax), ";cos #theta_{Lab}; #varphi_{Lab} (#circ);acceptance", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins, phiMin, phiMax);
 	TEfficiency* hCS = new TEfficiency(Form("CS_pt%dto%dGeV", ptMin, ptMax), ";cos #theta_{CS}; #varphi_{CS} (#circ);acceptance", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins, phiMin, phiMax);
 	TEfficiency* hHX = new TEfficiency(Form("HX_pt%dto%dGeV", ptMin, ptMax), ";cos #theta_{HX}; #varphi_{HX} (#circ);acceptance", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins, phiMin, phiMax);
 
@@ -76,6 +81,7 @@ void acceptanceMap_noGenFilter(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 
 
 	Long64_t totEntries = OniaTree->GetEntries();
 
+	// Loop over the events
 	for (Long64_t iEvent = 0; iEvent < (totEntries); iEvent++) {
 		if (iEvent % 10000 == 0) {
 			cout << Form("\rProcessing event %lld / %lld (%.0f%%)", iEvent, totEntries, 100. * iEvent / totEntries) << flush;
@@ -93,21 +99,21 @@ void acceptanceMap_noGenFilter(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 
 
 			// single-muon acceptance cuts
 			gen_mumi_LV = (TLorentzVector*)Gen_QQ_mumi_4mom->At(iGen);
-
 			gen_mupl_LV = (TLorentzVector*)Gen_QQ_mupl_4mom->At(iGen);
 
 			withinAcceptance = (fabs(gen_mupl_LV->Eta()) < 2.4) && (gen_mupl_LV->Pt() > 3.5) && (fabs(gen_mumi_LV->Eta()) < 2.4) && (gen_mumi_LV->Pt() > 3.5);
 
+			// Reference frame transformations
 			TVector3 muPlus_CS = MuPlusVector_CollinsSoper(*gen_QQ_LV, *gen_mupl_LV);
-
-			hCS->Fill(withinAcceptance, muPlus_CS.CosTheta(), muPlus_CS.Phi() * 180 / TMath::Pi());
-
 			TVector3 muPlus_HX = MuPlusVector_Helicity(*gen_QQ_LV, *gen_mupl_LV);
-
-			hHX->Fill(withinAcceptance, muplPvecBoosted.CosTheta(), muplPvecBoosted.Phi() * 180 / TMath::Pi());
+			
+			hLab->Fill(withinAcceptance, gen_mupl_LV->CosTheta(), gen_mupl_LV->Phi()*180/TMath::Pi());	
+			hCS->Fill(withinAcceptance, muPlus_CS.CosTheta(), muPlus_CS.Phi() * 180 / TMath::Pi());
+			hHX->Fill(withinAcceptance, muPlus_HX.CosTheta(), muPlus_HX.Phi() * 180 / TMath::Pi());
 		}
 	}
 
+	// Set the plot styles
 	gStyle->SetPadLeftMargin(.15);
 	//gStyle->SetTitleYOffset(.9);
 	gStyle->SetPadRightMargin(0.18);
@@ -118,6 +124,25 @@ void acceptanceMap_noGenFilter(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 
 	legend->SetTextAlign(22);
 	legend->SetTextSize(0.05);
 
+
+	// Draw and save the acceptance map for Lab frame
+	auto* canvasLab = new TCanvas("canvasLab", "", 700, 600);
+	hLab->Draw("COLZ");
+
+	CMS_lumi(canvasLab, Form("#varUpsilon(%dS) Pythia 8 + EvtGen MC, no filter", iState));
+
+	legend->DrawLatexNDC(.48, .88, Form("|y^{#mu#mu}| < 2.4, %d < p_{T}^{#mu#mu} < %d GeV", ptMin, ptMax));
+	legend->DrawLatexNDC(.48, .8, Form("#varUpsilon(%dS) acc. for |#eta^{#mu}| < 2.4, p_{T}^{#mu} > 3.5 GeV", iState));
+
+	gPad->Update();
+
+	hLab->GetPaintedHistogram()->GetYaxis()->SetRangeUser(-190, 300);
+	hLab->GetPaintedHistogram()->GetZaxis()->SetRangeUser(0, 1);
+
+	canvasLab->SaveAs(Form("AcceptanceMaps/%dS/Lab_pt%dto%dGeV.png", iState, ptMin, ptMax), "RECREATE");
+
+
+	// Draw and save the acceptance map for CS frame
 	auto* canvasCS = new TCanvas("canvasCS", "", 700, 600);
 	hCS->Draw("COLZ");
 
@@ -133,6 +158,8 @@ void acceptanceMap_noGenFilter(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 
 
 	canvasCS->SaveAs(Form("AcceptanceMaps/%dS/CS_pt%dto%dGeV.png", iState, ptMin, ptMax), "RECREATE");
 
+
+	// Draw and save the acceptance map for HX frame
 	auto* canvasHX = new TCanvas("canvasHX", "", 700, 600);
 	hHX->Draw("COLZ");
 
@@ -148,10 +175,12 @@ void acceptanceMap_noGenFilter(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 
 
 	canvasHX->SaveAs(Form("AcceptanceMaps/%dS/HX_pt%dto%dGeV.png", iState, ptMin, ptMax), "RECREATE");
 
+
 	/// save the results in a file for later usage (in particular for the polarization extraction)
 	const char* outputFileName = Form("AcceptanceMaps/%dS/AcceptanceResults.root", iState);
 	TFile outputFile(outputFileName, "RECREATE");
 
+	hLab->Write();
 	hCS->Write();
 	hHX->Write();
 
