46633b76d11759ebcb522cc60a6a1cbd3bc2d862
Saeahram <85007865+ashleyahram@users.noreply.github.com>
2023-07-07 17:14:15 +0200

    Fixed bugs, updated to save more acceptance maps
    Bugs:
    1. weight equation was wrong (wrong parenthesis location:'()
    2. acceptance was not properly applied (didn't put the parenthesis around it)
    3. dimuon pT cut was not applied properly
    4. legend of denominator of acceptance map
    
    Updates
    1. draw more figures with different polarization parameters
    2. save numerator histograms in HX and CS as well

--- MonteCarlo/acceptanceMap_GenWeights.C
+++ MonteCarlo/acceptanceMap_GenWeights.C
@@ -47,7 +47,6 @@ void acceptanceMap_GenWeights(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 3
 
 	TNtuple *HXCSNTuple = (TNtuple*)gDirectory -> Get("HXCSNTuple");
 
-
 	// Put the text, "CMS Internal", on the right top of the plot 
 	// writeExtraText = true; // if extra text
 	// extraText = "       Internal";
@@ -96,6 +95,8 @@ void acceptanceMap_GenWeights(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 3
 	// Define Muon acceptance cuts
 	const char* SingleMuonAccCuts = "(muplPtLab>3.5)&&(mumiPtLab>3.5)&&(abs(muplEtaLab)<2.4)&&(abs(mumiEtaLab)<2.4)";
 
+	char DoubleMuonCuts[50] = {};
+	sprintf(DoubleMuonCuts, "(upsPtLab>%d)&&(upsPtLab<%d)&&(abs(upsRapLab)<2.4)", ptMin, ptMax);
 
 	// Set the plot styles
 	gStyle -> SetPadLeftMargin(.15);
@@ -113,29 +114,22 @@ void acceptanceMap_GenWeights(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 3
 	// Set the weights
 	// Test1 values: λ_θ(HX)=0.88, λ_φ(HX)=-0.99, λ_θ(CS)=0.00, λ_φ(CS)=-0.50
 	// Test2 values: λ_θ(HX)=-1.00, λ_φ(HX)=0.00, λ_θ(CS)=-1.00, λ_φ(CS)=0.00	
-	double weights_test1[2][2] = {{0.88, -0.99}, {0.00, -0.50}}; 
-	double weights_test2[2][2] = {{-1.00, 0.00}, {-1.00, 0.00}}; 
-	// cout << weights_test1[0][0] << endl;
 
 	// Start1 values: λ_θ(HX)=1.00, λ_φ(HX)=1.00, λ_θ(CS)=1.00, λ_φ(CS)=1.00
 	// Start2 values: λ_θ(HX)=0.00, λ_φ(HX)=0.00, λ_θ(CS)=0.00, λ_φ(CS)=0.00
 	// Start3 values: λ_θ(HX)=1.00, λ_φ(HX)=0.00, λ_θ(CS)=1.00, λ_φ(CS)=0.00
 	// Start4 values: λ_θ(HX)=0.00, λ_φ(HX)=-0.50, λ_θ(CS)=0.00, λ_φ(CS)=-0.50	
-	double weights_start1[2][2] = {{1.00, 1.00}, {1.00, 1.00}}; 
-	double weights_start2[2][2] = {{0.00, 0.00}, {0.00, 0.00}}; 
-	double weights_start3[2][2] = {{1.00, 0.00}, {1.00, 0.00}}; 
-	double weights_start4[2][2] = {{0.00, -0.50}, {0.00, -0.50}}; 
-	// cout << weights_sart1[0][0] << endl;
 
+	const int NumIdx = 6;
+	double weights[NumIdx][2][2] = {{{0.88, -0.99}, {0.00, -0.50}}, //test1 --idx:0
+							   {{-1.00, 0.00}, {-1.00, 0.00}}, //test2 --idx:1
+							   {{1.00, 1.00}, {1.00, 1.00}}, //start1 --idx:2
+							   {{0.00, 0.00}, {0.00, 0.00}}, //start2 --idx:3
+							   {{1.00, 0.00}, {1.00, 0.00}}, //start3 --idx:4
+							   {{0.00, -0.50}, {0.00, -0.50}}}; //start4 --idx:5
 
-	// // ******** Set lambda_theta and lambda_phi values to be used for the acceptance plots ******** //
-
-	const int NumIdx = 1; //number of test values for lambda_theta and lambda_phi
-
-	// double lambda_theta[NumIdx] = {-1, -0.7, -0.5, -0.2, 0, 0.2, 0.5, 0.7, 1};
-	// // double lambda_theta = 1;
-	// double lambda_phi[NumIdx] = {-1, -0.7, -0.5, -0.2, 0, 0.2, 0.5, 0.7, 1};
-
+	char* weightsLabel[NumIdx] = {"Test1", "Test2", "Start1", "Start2", "Start3", "Start4"};
+	cout << weightsLabel[1] << endl;
 	// Define histograms for numerator and denominator in acceptance//
 	TH2F *hHXNum[NumIdx] = {0};
 	TH2F *hHXDen[NumIdx] = {0};
@@ -158,21 +152,21 @@ void acceptanceMap_GenWeights(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 3
 
 		// (For HX with the first, second order weight (lambda_theta, lambda_phi) (1+ lambda_theta*cos^2(theta)+ lambda_phi*sin^2(theta) * cos(2*phi)))
 		char hHXNumName[50]={},hHXDenName[50]={},hHXAccpName[50]={}, cHXNumName[50]={}, cHXDenName[50]={}, cHXAccpName[50]={};
-		sprintf(hHXNumName, "hHXNum%d", idx);
-		sprintf(hHXDenName, "hHXDen%d", idx);
-		sprintf(hHXAccpName, "hHXAccp%d", idx);
-		sprintf(cHXNumName, "cHXNum%d", idx);
-		sprintf(cHXDenName, "cHXDen%d", idx);
-		sprintf(cHXAccpName, "cHXAccp%d", idx);
+		sprintf(hHXNumName, "hHXNum%s", weightsLabel[idx]);
+		sprintf(hHXDenName, "hHXDen%s", weightsLabel[idx]);
+		sprintf(hHXAccpName, "hHXAccp%s", weightsLabel[idx]);
+		sprintf(cHXNumName, "cHXNum%s", weightsLabel[idx]);
+		sprintf(cHXDenName, "cHXDen%s", weightsLabel[idx]);
+		sprintf(cHXAccpName, "cHXAccp%s", weightsLabel[idx]);
 
 		// (For CS with the first, second order weight (lambda_theta, lambda_phi) (1+ lambda_theta*cos^2(theta)+ lambda_phi*sin^2(theta) * cos(2*phi)))
 		char hCSNumName[50]={},hCSDenName[50]={},hCSAccpName[50]={}, cCSNumName[50]={}, cCSDenName[50]={}, cCSAccpName[50]={};
-		sprintf(hCSNumName, "hCSNum%d", idx);
-		sprintf(hCSDenName, "hCSDen%d", idx);
-		sprintf(hCSAccpName, "hCSAccp%d", idx);
-		sprintf(cCSNumName, "cCSNum%d", idx);
-		sprintf(cCSDenName, "cCSDen%d", idx);
-		sprintf(cCSAccpName, "cCSAccp%d", idx);
+		sprintf(hCSNumName, "hCSNum%s", weightsLabel[idx]);
+		sprintf(hCSDenName, "hCSDen%s", weightsLabel[idx]);
+		sprintf(hCSAccpName, "hCSAccp%s", weightsLabel[idx]);
+		sprintf(cCSNumName, "cCSNum%s", weightsLabel[idx]);
+		sprintf(cCSDenName, "cCSDen%s", weightsLabel[idx]);
+		sprintf(cCSAccpName, "cCSAccp%s", weightsLabel[idx]);
 
 
 		// (For HX with the first, second order weight (lambda_theta, lambda_phi) (1+ lambda_theta*cos^2(theta)+ lambda_phi*sin^2(theta) * cos(2*phi)))
@@ -181,41 +175,42 @@ void acceptanceMap_GenWeights(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 3
 		hHXNum[idx] = new TH2F(hHXNumName, ";cos #theta_{HX}; #varphi_{HX} (#circ);Number of generated #varUpsilons", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins, phiMin, phiMax);
 		
 		// (apply weights, cuts and draw 2D graph on the phi-costheta phase space)
-		HXCSNTuple -> Draw(Form("muplPhiHX:muplCosThetaHX>>hHXNum%d", idx), Form("(1 + %f*pow(muplCosThetaHX, 2.0) + %f*(1- pow(muplCosThetaHX, 2.0) * cos(2*muplPhiHX)))*%s", weights_start1[0][0], weights_start1[0][1], SingleMuonAccCuts), "COLZ");
+		HXCSNTuple -> Draw(Form("muplPhiHX:muplCosThetaHX>>hHXNum%s", weightsLabel[idx]), Form("(1 + %f*pow(muplCosThetaHX, 2.0) + %f*(1- pow(muplCosThetaHX, 2.0))*cos(2*muplPhiHX) )*(%s&&%s)", weights[idx][0][0], weights[idx][0][1], SingleMuonAccCuts, DoubleMuonCuts), "COLZ");
+		// HXCSNTuple -> Draw(Form("muplPhiHX:muplCosThetaHX>>hHXNum%s", weightsLabel[idx]), "(1 + 0.88*pow(muplCosThetaHX, 2.0) -0.99*(1- pow(muplCosThetaHX, 2.0))*cos(2*muplPhiHX) )*((muplPtLab>3.5)&&(mumiPtLab>3.5)&&(abs(muplEtaLab)<2.4)&&(abs(mumiEtaLab)<2.4))", "COLZ");
 		// Put text upper right conner of the plot
 		CMS_lumi(cHXNum[idx], Form("#varUpsilon(%dS) Pythia 8 + EvtGen MC, no filter", iState));
 
 		// Put texts inside the plot
 		legend -> DrawLatexNDC(.48, .90, Form("|y^{#mu#mu}| < 2.4, %d < p_{T}^{#mu#mu} < %d GeV", ptMin, ptMax));
 		legend -> DrawLatexNDC(.48, .84, Form("#varUpsilon(%dS) acc. for |#eta^{#mu}| < 2.4, p_{T}^{#mu} > 3.5 GeV", iState));
-		legend -> DrawLatexNDC(.48, .78, Form("#lambda_{#theta, HX} = %.2f, #lambda_{#phi, HX} = %.2f", weights_start1[0][0], weights_start1[0][1]));
+		legend -> DrawLatexNDC(.48, .78, Form("#lambda_{#theta, HX} = %.2f, #lambda_{#phi, HX} = %.2f", weights[idx][0][0], weights[idx][0][1]));
 
 		gPad -> Update();
 
 		hHXNum[idx] -> GetYaxis() -> SetRangeUser(-190, 300);
 	
 		// (Save the canvas)
-		cHXNum[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/HXNum_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights_start1[0][0], weights_start1[0][1], ptMin, ptMax), "RECREATE");
+		cHXNum[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/HXNum_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights[idx][0][0], weights[idx][0][1], ptMin, ptMax), "RECREATE");
 
 
 		// (dinominator)
 		cHXDen[idx] = new TCanvas(cHXDenName, cHXDenName, 700, 600);
 		hHXDen[idx] = new TH2F(hHXDenName, ";cos #theta_{HX}; #varphi_{HX} (#circ);Number of generated #varUpsilons", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins, phiMin, phiMax);
 
-		HXCSNTuple -> Draw(Form("muplPhiHX:muplCosThetaHX>>hHXDen%d",idx), Form("(1+ %f*pow(muplCosThetaHX, 2.0)+ %f*(1- pow(muplCosThetaHX, 2.0) * cos(2*muplPhiHX)))", weights_start1[0][0], weights_start1[0][1]), "COLZ");
+		HXCSNTuple -> Draw(Form("muplPhiHX:muplCosThetaHX>>hHXDen%s", weightsLabel[idx]), Form("(1+ %f*pow(muplCosThetaHX, 2.0)+ %f*(1- pow(muplCosThetaHX, 2.0)) * cos(2*muplPhiHX))*(%s)", weights[idx][0][0], weights[idx][0][1], DoubleMuonCuts), "COLZ");
 		// Put text upper right conner of the plot
 		CMS_lumi(cHXDen[idx], Form("#varUpsilon(%dS) Pythia 8 + EvtGen MC, no filter", iState));
 
 		// Put texts inside the plot
 		legend -> DrawLatexNDC(.48, .90, Form("|y^{#mu#mu}| < 2.4, %d < p_{T}^{#mu#mu} < %d GeV", ptMin, ptMax));
-		legend -> DrawLatexNDC(.48, .84, Form("#varUpsilon(%dS) acc. for |#eta^{#mu}| < 2.4, p_{T}^{#mu} > 3.5 GeV", iState));
-		legend -> DrawLatexNDC(.48, .78, Form("#lambda_{#theta, HX} = %.2f, #lambda_{#phi, HX} = %.2f", weights_start1[0][0], weights_start1[0][1]));
+		// legend -> DrawLatexNDC(.48, .84, Form("#varUpsilon(%dS) acc. for |#eta^{#mu}| < 2.4, p_{T}^{#mu} > 3.5 GeV", iState));
+		legend -> DrawLatexNDC(.48, .84, Form("#lambda_{#theta, HX} = %.2f, #lambda_{#phi, HX} = %.2f", weights[idx][0][0], weights[idx][0][1]));
 
 		gPad -> Update();
 
 		hHXDen[idx] -> GetYaxis() -> SetRangeUser(-190, 300);
 
-		cHXDen[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/HXDen_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights_start1[0][0], weights_start1[0][1], ptMin, ptMax), "RECREATE");
+		cHXDen[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/HXDen_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights[idx][0][0], weights[idx][0][1], ptMin, ptMax), "RECREATE");
 
 
 
@@ -225,40 +220,40 @@ void acceptanceMap_GenWeights(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 3
 		hCSNum[idx] = new TH2F(hCSNumName, ";cos #theta_{CS}; #varphi_{CS} (#circ);Number of generated #varUpsilons", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins, phiMin, phiMax);
 		
 		// (apply weights, cuts and draw 2D graph on the phi-costheta phase space)
-		HXCSNTuple -> Draw(Form("muplPhiCS:muplCosThetaCS>>hCSNum%d", idx), Form("(1 + %f*pow(muplCosThetaCS, 2.0) + %f*(1- pow(muplCosThetaCS, 2.0) * cos(2*muplPhiCS)))*%s", weights_start1[1][0], weights_start1[1][1], SingleMuonAccCuts), "COLZ");
+		HXCSNTuple -> Draw(Form("muplPhiCS:muplCosThetaCS>>hCSNum%s", weightsLabel[idx]), Form("(1 + %f*pow(muplCosThetaCS, 2.0) + %f*(1- pow(muplCosThetaCS, 2.0)) * cos(2*muplPhiCS))*(%s&&%s)", weights[idx][1][0], weights[idx][1][1], SingleMuonAccCuts, DoubleMuonCuts), "COLZ");
 		// Put text upper right conner of the plot
 		CMS_lumi(cCSNum[idx], Form("#varUpsilon(%dS) Pythia 8 + EvtGen MC, no filter", iState));
 
 		// Put texts inside the plot
 		legend -> DrawLatexNDC(.48, .90, Form("|y^{#mu#mu}| < 2.4, %d < p_{T}^{#mu#mu} < %d GeV", ptMin, ptMax));
 		legend -> DrawLatexNDC(.48, .84, Form("#varUpsilon(%dS) acc. for |#eta^{#mu}| < 2.4, p_{T}^{#mu} > 3.5 GeV", iState));
-		legend -> DrawLatexNDC(.48, .78, Form("#lambda_{#theta, CS} = %.2f, #lambda_{#phi, CS} = %.2f", weights_start1[1][0], weights_start1[1][1]));
+		legend -> DrawLatexNDC(.48, .78, Form("#lambda_{#theta, CS} = %.2f, #lambda_{#phi, CS} = %.2f", weights[idx][1][0], weights[idx][1][1]));
 
 		gPad -> Update();
 
 		hCSNum[idx] -> GetYaxis() -> SetRangeUser(-190, 300);
 	
 		// (Save the canvas)
-		cCSNum[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/CSNum_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights_start1[1][0], weights_start1[1][1], ptMin, ptMax), "RECREATE");
+		cCSNum[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/CSNum_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights[idx][1][0], weights[idx][1][1], ptMin, ptMax), "RECREATE");
 
 		// (dinominator)
 		cCSDen[idx] = new TCanvas(cCSDenName, cCSDenName, 700, 600);
 		hCSDen[idx] = new TH2F(hCSDenName, ";cos #theta_{CS}; #varphi_{CS} (#circ);Number of generated #varUpsilons", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins, phiMin, phiMax);
 
-		HXCSNTuple -> Draw(Form("muplPhiCS:muplCosThetaCS>>hCSDen%d",idx), Form("(1+ %f*pow(muplCosThetaCS, 2.0)+ %f*(1- pow(muplCosThetaCS, 2.0) * cos(2*muplPhiCS)))", weights_start1[1][0], weights_start1[1][1]), "COLZ");
+		HXCSNTuple -> Draw(Form("muplPhiCS:muplCosThetaCS>>hCSDen%s", weightsLabel[idx]), Form("(1+ %f*pow(muplCosThetaCS, 2.0)+ %f*(1- pow(muplCosThetaCS, 2.0)) * cos(2*muplPhiCS))*(%s)", weights[idx][1][0], weights[idx][1][1], DoubleMuonCuts), "COLZ");
 		// Put text upper right conner of the plot
 		CMS_lumi(cCSDen[idx], Form("#varUpsilon(%dS) Pythia 8 + EvtGen MC, no filter", iState));
 
 		// Put texts inside the plot
 		legend -> DrawLatexNDC(.48, .90, Form("|y^{#mu#mu}| < 2.4, %d < p_{T}^{#mu#mu} < %d GeV", ptMin, ptMax));
-		legend -> DrawLatexNDC(.48, .84, Form("#varUpsilon(%dS) acc. for |#eta^{#mu}| < 2.4, p_{T}^{#mu} > 3.5 GeV", iState));
-		legend -> DrawLatexNDC(.48, .78, Form("#lambda_{#theta, CS} = %.2f, #lambda_{#phi, CS} = %.2f", weights_start1[1][0], weights_start1[1][1]));
+		// legend -> DrawLatexNDC(.48, .84, Form("#varUpsilon(%dS) acc. for |#eta^{#mu}| < 2.4, p_{T}^{#mu} > 3.5 GeV", iState));
+		legend -> DrawLatexNDC(.48, .84, Form("#lambda_{#theta, CS} = %.2f, #lambda_{#phi, CS} = %.2f", weights[idx][1][0], weights[idx][1][1]));
 
 		gPad -> Update();
 
 		hCSDen[idx] -> GetYaxis() -> SetRangeUser(-190, 300);
 	
-		cCSDen[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/CSDen_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights_start1[1][0], weights_start1[1][1], ptMin, ptMax), "RECREATE");
+		cCSDen[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/CSDen_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights[idx][1][0], weights[idx][1][1], ptMin, ptMax), "RECREATE");
 
 
 
@@ -270,11 +265,11 @@ void acceptanceMap_GenWeights(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 3
 		for(int iThetaBin=1; iThetaBin<=nCosThetaBins; iThetaBin++){
 			for(int iPhiBin=1; iPhiBin<=nPhiBins; iPhiBin++){
 				// (For HX with the first, second order weight (lambda_theta, lambda_phi) (1+ lambda_theta*cos^2(theta)+ lambda_phi*sin^2(theta) * cos(2*phi)))
-				if(hHXDen[idx]->GetBinContent(iThetaBin, iPhiBin)==0) continue;
+				if((hHXDen[idx]->GetBinContent(iThetaBin, iPhiBin))==0) continue;
 				hHXAccp[idx] -> SetBinContent(iThetaBin, iPhiBin, hHXNum[idx]->GetBinContent(iThetaBin,iPhiBin)/hHXDen[idx]->GetBinContent(iThetaBin,iPhiBin));
 
 				// (For CS with the first, second order weight (lambda_theta, lambda_phi) (1+ lambda_theta*cos^2(theta)+ lambda_phi*sin^2(theta) * cos(2*phi)))
-				if(hCSDen[idx]->GetBinContent(iThetaBin,iPhiBin)==0) continue;
+				if((hCSDen[idx]->GetBinContent(iThetaBin,iPhiBin))==0) continue;
 				hCSAccp[idx] -> SetBinContent(iThetaBin,iPhiBin, hCSNum[idx]->GetBinContent(iThetaBin,iPhiBin)/hCSDen[idx]->GetBinContent(iThetaBin,iPhiBin));
 			}
 		}
@@ -287,13 +282,13 @@ void acceptanceMap_GenWeights(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 3
 		// Put texts inside the plot
 		legend -> DrawLatexNDC(.48, .90, Form("|y^{#mu#mu}| < 2.4, %d < p_{T}^{#mu#mu} < %d GeV", ptMin, ptMax));
 		legend -> DrawLatexNDC(.48, .84, Form("#varUpsilon(%dS) acc. for |#eta^{#mu}| < 2.4, p_{T}^{#mu} > 3.5 GeV", iState));
-		legend -> DrawLatexNDC(.48, .78, Form("#lambda_{#theta, HX} = %.2f, #lambda_{#phi, HX} = %.2f", weights_start1[0][0], weights_start1[0][1]));
+		legend -> DrawLatexNDC(.48, .78, Form("#lambda_{#theta, HX} = %.2f, #lambda_{#phi, HX} = %.2f", weights[idx][0][0], weights[idx][0][1]));
 
 		gPad -> Update();
 
 		hHXAccp[idx] -> GetYaxis() -> SetRangeUser(-190, 300);
 		hHXAccp[idx] -> GetZaxis() -> SetRangeUser(0, 1);
-		cHXAccp[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/HXAccp_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights_start1[0][0], weights_start1[0][1], ptMin, ptMax), "RECREATE");
+		cHXAccp[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/HXAccp_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights[idx][0][0], weights[idx][0][1], ptMin, ptMax), "RECREATE");
 
 
 		cCSAccp[idx] = new TCanvas(cCSAccpName, cCSAccpName, 700, 600);
@@ -304,13 +299,13 @@ void acceptanceMap_GenWeights(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 3
 		// Put texts inside the plot
 		legend -> DrawLatexNDC(.48, .90, Form("|y^{#mu#mu}| < 2.4, %d < p_{T}^{#mu#mu} < %d GeV", ptMin, ptMax));
 		legend -> DrawLatexNDC(.48, .84, Form("#varUpsilon(%dS) acc. for |#eta^{#mu}| < 2.4, p_{T}^{#mu} > 3.5 GeV", iState));
-		legend -> DrawLatexNDC(.48, .78, Form("#lambda_{#theta, CS} = %.2f, #lambda_{#phi, CS} = %.2f", weights_start1[1][0], weights_start1[1][1]));
+		legend -> DrawLatexNDC(.48, .78, Form("#lambda_{#theta, CS} = %.2f, #lambda_{#phi, CS} = %.2f", weights[idx][1][0], weights[idx][1][1]));
 
 		gPad -> Update();
 
 		hCSAccp[idx] -> GetYaxis() -> SetRangeUser(-190, 300);
 		hCSAccp[idx] -> GetZaxis() -> SetRangeUser(0, 1);
-		cCSAccp[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/CSAccp_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights_start1[1][0], weights_start1[1][1], ptMin, ptMax), "RECREATE");
+		cCSAccp[idx] -> SaveAs(Form("AcceptanceMaps/%dS/withWeights/CSAccp_Theta%.2f_Phi%.2f_pt%dto%dGeV.png", iState, weights[idx][1][0], weights[idx][1][1], ptMin, ptMax), "RECREATE");
 
 
 	}
@@ -320,8 +315,10 @@ void acceptanceMap_GenWeights(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 3
 	TFile outputFile(outputFileName, "RECREATE");
 
 	for(int idx=0; idx<NumIdx; idx++){
-		hCSAccp[idx]->Write();
-		hHXAccp[idx]->Write();
+		hCSNum[idx] -> Write();
+		hHXNum[idx] -> Write();
+		hCSAccp[idx] -> Write();
+		hHXAccp[idx] -> Write();
 	}
 
 	outputFile.Close();
