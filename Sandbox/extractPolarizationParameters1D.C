0102da4a944bf4813a183e664e9379f39c39efd9
ashleyahram <ashleyahram@naver.com>
2023-07-13 23:24:37 +0200

    Extract Polarization Parameters w/ 1D fit
    Using this code, we can extract the polarization parameters from 2D costheta, phi phase space plots (made by angular distribution function in this code) with 1D fit. New code is being written for 2D fit. Probably I need to switch to ROOFIT instead of using fit function in ROOT.

---
+++ Sandbox/extractPolarizationParameters1D.C
@@ -0,0 +1,207 @@
+// This code extracts the polarization parameters 
+// from the 2D angular distribution function (ideal distribution for the test) in the costheta phi space 
+// using 1D fit
+
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TFile.h"
+#include "TNtuple.h"
+#include "TF1.h"
+#include "TH2.h"
+#include "TLine.h"
+#include "TCanvas.h"
+#include "TRandom3.h"
+#include "TVector3.h"
+#include "TRotation.h"
+#include "TLorentzVector.h"
+#include "TGraph.h"
+#include "TString.h"
+#include "TLatex.h"
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+#include "../Tools/Style/tdrStyle.C"
+#include "../Tools/Style/CMS_lumi.C"
+
+#include "../Tools/Style/FitDistributions.h"
+#include "../Tools/Style/Legends.h"
+
+
+void extractPolarizationParameters1D() {  
+// void extractPolarizationParameters1D(Int_t iState = 1, Int_t ptMin = 0, Int_t ptMax = 30) {  
+
+	/// Start measuring time 
+	clock_t start, end, cpu_time;
+	start = clock();
+
+
+	/// Put the text, "CMS Internal", on the right top of the plot 
+	// writeExtraText = true; // if extra text
+	// extraText = "       Internal";
+
+	writeExtraText = false; // if don't want to put a text
+
+	/// Generate a Toy Data (This part can be replaced by data)
+	// (cos theta, phi) 2D distribution maps for CS and HX frames
+	Int_t nCosThetaBins = 20;
+	Float_t cosThetaMin = -1, cosThetaMax = 1;
+
+	//	Double_t cosThetaBinning[] = {-1, -0.8, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1};
+	//	nCosThetaBins = sizeof(cosThetaBinning) / sizeof(Double_t) - 1;
+
+	Int_t nPhiBins = 18;
+	Float_t phiMin = -180, phiMax = 180;
+
+	//Double_t phiBinning[] = {-TMath::Pi(), -2.5, -2, -1.5, -1, -0.5, 0.5, 1, 1.5, 2, 2.5, 180};
+	//	nPhiBins = sizeof(cosThetaBinning) / sizeof(Double_t) - 1;
+
+
+	/// Set the plot styles
+	gStyle -> SetPadLeftMargin(.15);
+	gStyle -> SetPadRightMargin(.2);
+	//gStyle->SetTitleYOffset(.9);
+	gStyle -> SetTitleOffset(1.2,"z"); // gap between color bar and z title
+	gStyle -> SetPalette(kCividis);
+	gStyle -> SetNumberContours(256);
+
+	/// Styles of the texts in the plot
+	TLatex* legend = new TLatex();
+	legend -> SetTextAlign(22);
+	legend -> SetTextSize(0.05);
+
+
+	/// Set the weights
+	// Test1 values: λ_θ(HX)=0.88, λ_φ(HX)=-0.99, λ_θ(CS)=0.00, λ_φ(CS)=-0.50
+	// Test2 values: λ_θ(HX)=-1.00, λ_φ(HX)=0.00, λ_θ(CS)=-1.00, λ_φ(CS)=0.00	
+
+	// Start1 values: λ_θ(HX)=1.00, λ_φ(HX)=1.00, λ_θ(CS)=1.00, λ_φ(CS)=1.00
+	// Start2 values: λ_θ(HX)=0.00, λ_φ(HX)=0.00, λ_θ(CS)=0.00, λ_φ(CS)=0.00
+	// Start3 values: λ_θ(HX)=1.00, λ_φ(HX)=0.00, λ_θ(CS)=1.00, λ_φ(CS)=0.00
+	// Start4 values: λ_θ(HX)=0.00, λ_φ(HX)=-0.50, λ_θ(CS)=0.00, λ_φ(CS)=-0.50	
+
+	const int NumIdx = 6;
+	double weights[NumIdx][2][2] = {{{0.88, -0.99}, {0.00, -0.50}}, //test1 --idx:0
+							   {{-1.00, 0.00}, {-1.00, 0.00}}, //test2 --idx:1
+							   {{1.00, 1.00}, {1.00, 1.00}}, //start1 --idx:2
+							   {{0.00, 0.00}, {0.00, 0.00}}, //start2 --idx:3
+							   {{1.00, 0.00}, {1.00, 0.00}}, //start3 --idx:4
+							   {{0.00, -0.50}, {0.00, -0.50}}}; //start4 --idx:5
+
+
+
+	/// Define and draw 2D Angular distribution function in (costheta, phi) phase space
+	/// (I'm testing 2D fit with an ideal case. This part could be replaced with MC or Data)
+	/// (dummy histogram to adjust the plot range)
+	TH2F *hdummy = new TH2F("hdummy", ";cos #theta; #varphi (#circ);Number of generated #varUpsilons", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins+5, phiMin-20, phiMax+100); 
+
+	/// (2D Angular distribution function)
+	TF2  *AngDisFunc = new TF2("AngDisFunc","[2]/(3+[0])*(1+[0]*x*x+[1]*(1-x*x)*(cos(2*y*[3]/180.)))", cosThetaMin, cosThetaMax, phiMin, phiMax); 
+	AngDisFunc -> FixParameter(0, weights[1][0][0]); // (parameter of lambda theta)
+	AngDisFunc -> FixParameter(1, weights[1][0][1]); // (parameter of lambda phi)
+	AngDisFunc -> FixParameter(2, 1e2); // (parameter of normalization)
+	AngDisFunc -> FixParameter(3, M_PI); // (additional factor since y(#phi) is in the unit of degree)
+	AngDisFunc -> SetParNames("lambda_theta", "lambda_phi", "NormFactor", "pi");
+	AngDisFunc -> SetTitle(";cos #theta; #varphi (#circ);Number of generated #varUpsilons");
+	// AngDisfunc -> SetParLimits(0,-1,1);
+	// TFitResultPtr r; // Extract fit parameters
+	// TMatrixDSym cov;
+
+	/// (histogram for Random sampling from the angular distribution function)
+	TH2D *hAngDis = new TH2D("hAngDis", ";cos #theta; #varphi (#circ);Number of generated #varUpsilons", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins, phiMin, phiMax); 
+
+	hAngDis -> FillRandom("AngDisFunc", 1e7); // convert the fuction to histogram for fit procedure
+
+	/// Draw graphs
+	TCanvas *cAngDisFunc = new TCanvas("cAngDisFunc", "cAngDisFunc", 700, 600);
+	// hdummy -> GetZaxis() ->SetRangeUser(0, AngDisFunc->GetMaximum());
+	hdummy -> GetZaxis() ->SetRangeUser(0, hAngDis->GetMaximum());
+	hdummy -> Draw("COLZ");
+
+	// AngDisFunc -> Draw("COLZ same");
+	// AngDisFunc -> Draw("same");
+
+	hAngDis -> Draw("COLZ same");
+
+	/// Put text upper right conner of the plot
+	// CMS_lumi(FitFunc, "Angular distribution function");
+
+	/// Put texts inside the plot
+	legend -> DrawLatexNDC(.48, .88, "Angular distribution function (ideal)");
+	legend -> DrawLatexNDC(.48, .80, Form("#lambda_{#theta} = %.2f, #lambda_{#phi} = %.2f", AngDisFunc->GetParameter(0), AngDisFunc->GetParameter(1)));
+	
+	gPad -> Update();
+
+	// cAngDisFunc -> SaveAs(Form("Polarization2D/AngularDisributionFuctionTheta%.2f_Phi%.2f.png", AngDisFunc->GetParameter(0), AngDisFunc->GetParameter(1)), "RECREATE");
+
+
+
+	/// Extract Polarization Parameters with 1D Fit (costheta)
+	TCanvas *cAngDis1DCos = new TCanvas("cAngDis1DCos", "cAngDis1DCos", 700, 600);
+
+	/// Make 2D histogram to 1D (integrate over phi, that is, costheta graph)
+	TH1D *hAngDis1DCos = hAngDis -> ProjectionX("cos #theta", 1, nPhiBins);
+	hAngDis1DCos -> SetMinimum(0);
+	hAngDis1DCos -> Draw();
+
+	double sum;
+	for(int i=1; i<=nPhiBins; i++){
+		sum = 0;
+		for(int j=1; j<=nCosThetaBins; j++){
+			sum += (hAngDis -> GetBinContent(j, i));
+		}
+		// cout << "CosthetaBin number: " << i << ", sum: " << sum << endl;
+		cout << "PhiBin number: " << i << ", sum: " << sum << endl;
+	}
+	/// Define 1D Fit function with the first order (Costheta)
+	TF1  *FitFunc1DCos = new TF1("FitFunc1DCos","[1]*(1+[0]*x*x)/(3+[0])", cosThetaMin, cosThetaMax);
+	FitFunc1DCos -> SetParameter(0, 1); // parameter of lambda theta
+	FitFunc1DCos -> SetParameter(1, hAngDis->GetEntries()); // parameter of normalization
+	FitFunc1DCos -> SetParNames("lambda_theta", "NormFactor");
+	// FitFunc1DCos -> SetParLimits(0, -1, 1);
+	// TFitResultPtr r; // Extract fit parameters
+	// TMatrixDSym cov;
+
+	/// Fit the 1D random sampling histo to the 1D angular distribution function
+	hAngDis1DCos -> Fit("FitFunc1DCos", "S");
+
+
+
+	/// Extract Polarization Parameters with 1D Fit (phi)
+	TCanvas *cAngDis1DPhi = new TCanvas("cAngDis1DPhi", "cAngDis1DPhi", 700, 600);
+	
+	/// Make 2D histogram to 1D (integrate over costheta, that is, phi graph)
+	TH1D *hAngDis1DPhi = hAngDis -> ProjectionY("#phi", 1, nCosThetaBins);
+	hAngDis1DPhi -> SetMinimum(0);
+	hAngDis1DPhi -> Draw();
+
+	/// Define 1D Fit function with the second order (Phi)
+	TF1  *FitFunc1DPhi = new TF1("FitFunc1DPhi","[2]*(1+[1]/2.*cos(2*x* [3]/180.))/(3+[0])", phiMin, phiMax);
+	FitFunc1DPhi -> FixParameter(0, FitFunc1DCos->GetParameter(0)); // parameter of lambda theta
+	FitFunc1DPhi -> SetParameter(1, 1); // parameter of lambda phi
+	FitFunc1DPhi -> SetParameter(2, hAngDis->GetEntries()); // parameter of normalization
+	FitFunc1DPhi -> FixParameter(3, M_PI);
+	FitFunc1DPhi -> SetParNames("lambda_theta", "lambda_phi", "NormFactor", "pi");
+	// FitFunc1DPhi -> SetParLimits(0, -1, 1);
+	// FitFunc1DPhi -> SetParLimits(1, -1, 1); // parameter of lambda phi
+	// TFitResultPtr r; // Extract fit parameters
+	// TMatrixDSym cov;
+
+	// FitFunc1DPhi -> Draw("same");
+
+	/// Fit the 1D random sampling histo to the 1D angular distribution function
+	hAngDis1DPhi -> Fit("FitFunc1DPhi", "S");
+
+
+	/// End measuring time
+	end = clock();
+	cpu_time = (double)(end - start)/CLOCKS_PER_SEC;
+	cout << "Time elapsed: " << cpu_time/60. << "minutes" << endl;
+
+
+	return;
+
+
+
+	}
