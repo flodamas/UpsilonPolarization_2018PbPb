e66677f38e7ada2afa3345b12f94757dba82341a
ashleyahram <ashleyahram@naver.com>
2023-07-25 20:53:06 +0200

    Updated with pull distribution
    Updated the code with a pull distribution. Still need to fix the fit procedure and an error in the normalization factor fit parameter

--- Sandbox/extractPolarizationParameters1D.C
+++ Sandbox/extractPolarizationParameters1D.C
@@ -23,6 +23,7 @@
 #include "TGraph.h"
 #include "TString.h"
 #include "TLatex.h"
+#include "TAxis.h"
 
 ///RooFit
 #include "RooDataSet.h"
@@ -78,20 +79,17 @@ void extractPolarizationParameters1D() {
 	//Double_t phiBinning[] = {-TMath::Pi(), -2.5, -2, -1.5, -1, -0.5, 0.5, 1, 1.5, 2, 2.5, 180};
 	//	nPhiBins = sizeof(cosThetaBinning) / sizeof(Double_t) - 1;
 
-
 	/// Set the plot styles
-	gStyle -> SetPadLeftMargin(.15);
-	gStyle -> SetPadRightMargin(.2);
-	//gStyle->SetTitleYOffset(.9);
-	gStyle -> SetTitleOffset(1.2,"z"); // gap between color bar and z title
+	//gStyle -> SetTitleYOffset(.9);
+	gStyle -> SetTitleOffset(1.5,"z"); // gap between color bar and z title
 	gStyle -> SetPalette(kCividis);
 	gStyle -> SetNumberContours(256);
 
-	/// Styles of the texts in the plot
-	TLatex* legend = new TLatex();
-	legend -> SetTextAlign(22);
-	legend -> SetTextSize(0.05);
-
+	/// Define and draw 2D Angular distribution function in (costheta, phi) phase space
+	/// (I'm testing 2D fit with an ideal case. This part could be replaced with MC or Data later)	
+    ///----------------------------------------------------------------------------------------
+	/// (dummy histogram to adjust the plot range)
+	TH2F *hdummy = new TH2F("hdummy", ";cos #theta; #varphi (#circ);Number of generated #varUpsilons", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins+5, phiMin-20, phiMax+100); 
 
 	/// Set the weights
 	// Test1 values: λ_θ(HX)=0.88, λ_φ(HX)=-0.99, λ_θ(CS)=0.00, λ_φ(CS)=-0.50
@@ -110,15 +108,8 @@ void extractPolarizationParameters1D() {
 							   {{1.00, 0.00}, {1.00, 0.00}}, //start3 --idx:4
 							   {{0.00, -0.50}, {0.00, -0.50}}}; //start4 --idx:5
 
-
-
-	/// Define and draw 2D Angular distribution function in (costheta, phi) phase space
-	/// (I'm testing 2D fit with an ideal case. This part could be replaced with MC or Data)
-	/// (dummy histogram to adjust the plot range)
-	TH2F *hdummy = new TH2F("hdummy", ";cos #theta; #varphi (#circ);Number of generated #varUpsilons", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins+5, phiMin-20, phiMax+100); 
-
 	int chooseIdx = 0; // choose the index (0-5) above
-	int HXorCS = 0; // HX is 0, CS is 1
+	int HXorCS = 1; // HX is 0, CS is 1
 
 	/// (2D Angular distribution function)
 	TF2  *AngDisFunc = new TF2("AngDisFunc","[2]/(3+[0])*(1+[0]*x*x+[1]*(1-x*x)*(cos(2*y*[3]/180.)))", cosThetaMin, cosThetaMax, phiMin, phiMax); 
@@ -128,28 +119,32 @@ void extractPolarizationParameters1D() {
 	AngDisFunc -> FixParameter(3, M_PI); // (additional factor since y(#phi) is in the unit of degree)
 	AngDisFunc -> SetParNames("lambda_theta", "lambda_phi", "NormFactor", "pi");
 	AngDisFunc -> SetTitle(";cos #theta; #varphi (#circ);Number of generated #varUpsilons");
-	// AngDisfunc -> SetParLimits(0,-1,1);
-	// TFitResultPtr r; // Extract fit parameters
-	// TMatrixDSym cov;
 
 	/// (histogram for Random sampling from the angular distribution function)
 	TH2D *hAngDis = new TH2D("hAngDis", ";cos #theta; #varphi (#circ);Number of generated #varUpsilons", nCosThetaBins, cosThetaMin, cosThetaMax, nPhiBins, phiMin, phiMax); 
 
 	hAngDis -> FillRandom("AngDisFunc", 1e7); // convert the fuction to histogram for fit procedure
 
+
 	/// Draw graphs
 	TCanvas *cAngDisFunc = new TCanvas("cAngDisFunc", "cAngDisFunc", 700, 600);
-	// hdummy -> GetZaxis() ->SetRangeUser(0, AngDisFunc->GetMaximum());
+	
+	/// Set canvas margin
+	cAngDisFunc -> SetLeftMargin(.15);
+	cAngDisFunc -> SetRightMargin(.22);
+
 	hdummy -> GetZaxis() ->SetRangeUser(0, hAngDis->GetMaximum());
 	hdummy -> Draw("COLZ");
-
-	// AngDisFunc -> Draw("COLZ same");
-	// AngDisFunc -> Draw("same");
 
 	hAngDis -> Draw("COLZ same");
 
 	/// Put text upper right conner of the plot
 	// CMS_lumi(FitFunc, "Angular distribution function");
+
+	/// Styles of the texts in the plot
+	TLatex* legend = new TLatex();
+	legend -> SetTextAlign(22);
+	legend -> SetTextSize(0.05);
 
 	/// Put texts inside the plot
 	legend -> DrawLatexNDC(.48, .88, "Angular distribution function (ideal)");
@@ -159,21 +154,22 @@ void extractPolarizationParameters1D() {
 
 	// cAngDisFunc -> SaveAs(Form("Polarization2D/AngularDisributionFuctionTheta%.2f_Phi%.2f.png", AngDisFunc->GetParameter(0), AngDisFunc->GetParameter(1)), "RECREATE");
 
+    ///----------------------------------------------------------------------------------------
+
 
 	/// Extract Polarization Parameters with 1D Fit (costheta)
-	// TCanvas *cAngDis1DCos = new TCanvas("cAngDis1DCos", "cAngDis1DCos", 700, 600);
-
 	/// Make 2D histogram to 1D (integrate over phi, that is, costheta graph)
 	TH1D *hAngDis1DCos = hAngDis -> ProjectionX("cos #theta", 1, nPhiBins);
-	// hAngDis1DCos -> SetMinimum(0);
-	// hAngDis1DCos -> Draw();
 
 	/// Define variables
 	RooRealVar costheta("costheta", "cos#theta", cosThetaMin, cosThetaMax); 
 	RooRealVar phi("phi", "#phi (#circ)", phiMin, phiMax); //y
 
-	RooRealVar normCos("normCos", "normalization factor for costheta graph", hAngDis->GetEntries(), 0, 1e6); //normalization factor
-	RooRealVar normPhi("normPhi", "normalization factor for phi graph", hAngDis->GetEntries(), 0, 1e6); //normalization factor
+	RooRealVar normCos("normCos", "normalization factor for costheta graph", hAngDis->GetEntries(), 9.5e6, 1.5e7); //normalization factor
+	RooRealVar normPhi("normPhi", "normalization factor for phi graph", hAngDis->GetEntries(), 9.5e6, 1.5e7); //normalization factor
+	cout << "--------------------------------------" << endl;
+	cout << "entries: " << hAngDis -> GetEntries() << endl;
+	cout << "--------------------------------------" << endl;
 	RooRealVar lambTheta("lambTheta", "lambda Theta", 1, -1, 1);
 	RooRealVar lambPhi("lambPhi", "lambda Phi", 1, -1, 1);
 	RooRealVar pi("pi", "pi", M_PI);
@@ -185,18 +181,43 @@ void extractPolarizationParameters1D() {
 	RooGenericPdf AngDisFuncCosPdf("AngDisFuncCosPdf", "normCos*(1+lambTheta*costheta*costheta)/(3+lambTheta)", RooArgSet(costheta, normCos, lambTheta)); 
 
 	/// Make a frame
-	RooPlot* cosframe = costheta.frame();
-	cosHist.plotOn(cosframe);
+	RooPlot* cosframe = costheta.frame(cosThetaMin, cosThetaMax, nCosThetaBins);
+	cosHist.plotOn(cosframe, Name("cosHist"), MarkerSize(1.5), DrawOption("P0Z"));
 
 	/// Fit the model to the histogram
-	AngDisFuncCosPdf.fitTo(cosHist);
+	auto* fitResultCos = AngDisFuncCosPdf.fitTo(cosHist, Save(), Extended(kTRUE), Minos(kTRUE), NumCPU(3), Range(cosThetaMin, cosThetaMax));
+	fitResultCos -> Print("v");
 	/// Put the histogram on the frame
-	AngDisFuncCosPdf.plotOn(cosframe);
+	AngDisFuncCosPdf.plotOn(cosframe, Name("AngDisFuncCosPdf"));
+	
 
 	/// Draw the histogram and the fit
-	TCanvas* cCosHist = new TCanvas("cCosHist", "cCosHist", 700, 600);
-	cosframe->Draw();
-	cCosHist-> SaveAs(Form("CosTheta1D_lambCos%flambPhi%f.png", weights[chooseIdx][HXorCS][0], weights[chooseIdx][HXorCS][1]), "RECREATE");
+	TCanvas* cCosHist = new TCanvas("cCosHist", "cCosHist", 600, 600);
+	cCosHist -> cd();
+	
+	TPad *padCos1 = new TPad("padCos1", "padCos1", 0, 0.25, 1, 1.0);
+	padCos1 -> SetBottomMargin(0.03);
+	padCos1 -> SetTicks(1, 1);
+	padCos1 -> Draw();
+	padCos1 -> cd();
+	
+	// add legends
+	cosframe -> addObject(PolarParamsText(weights[chooseIdx][HXorCS][0], weights[chooseIdx][HXorCS][1], lambTheta, lambPhi, false));
+	cosframe -> GetXaxis() -> SetLabelOffset(1); // to make it disappear under the pull distribution pad
+	cosframe -> SetTitle("");
+	cosframe -> Draw();
+	gPad -> RedrawAxis();
+
+	// Pull Distribution
+	cCosHist -> cd();
+	
+	TPad* padCos2 = GetPadPullDistribution(cosframe, fitResultCos->floatParsFinal().getSize());
+
+	cCosHist -> cd();
+  	padCos1 -> Draw();
+  	padCos2 -> Draw();
+
+	cCosHist -> SaveAs(Form("CosTheta1D_lambCos%flambPhi%f.png", weights[chooseIdx][HXorCS][0], weights[chooseIdx][HXorCS][1]), "RECREATE");
 
 	cout << "--------------------------------------" << endl;
 	cout << "Done costheta fit!" << endl;
@@ -209,13 +230,9 @@ void extractPolarizationParameters1D() {
 	/// Fix lambda Theta and use this value for the phi graph fit
 	lambTheta.setConstant(kTRUE) ;
 
-	/// Extract Polarization Parameters with 1D Fit (phi)
-	// TCanvas *cAngDis1DPhi = new TCanvas("cAngDis1DPhi", "cAngDis1DPhi", 700, 600);
-	
+	/// Extract Polarization Parameters with 1D Fit (phi)	
 	/// Make 2D histogram to 1D (integrate over costheta, that is, phi graph)
 	TH1D *hAngDis1DPhi = hAngDis -> ProjectionY("#phi", 1, nCosThetaBins);
-	// hAngDis1DPhi -> SetMinimum(0);
-	// hAngDis1DPhi -> Draw();
 
 	/// Import histogram into RooFit
 	RooDataHist phiHist("phiHist","angular distribution with phi", phi, hAngDis1DPhi);
@@ -225,19 +242,43 @@ void extractPolarizationParameters1D() {
 
 	/// Make a frame
 	RooPlot* phiframe = phi.frame();
-	phiHist.plotOn(phiframe);
+	phiHist.plotOn(phiframe, Name("phiHist"), MarkerSize(1.5), DrawOption("P0Z"));
 
 	/// Fit the model to the histogram
-	AngDisFuncPhiPdf.fitTo(phiHist);
+	auto* fitResultPhi = AngDisFuncPhiPdf.fitTo(phiHist, Save(), Extended(kTRUE), Minos(kTRUE), NumCPU(3), Range(phiMin, phiMax));
+	fitResultPhi -> Print("v");
 	/// Put the histogram on the frame
-	AngDisFuncPhiPdf.plotOn(phiframe);
+	AngDisFuncPhiPdf.plotOn(phiframe, Name("AngDisFuncPhiPdf"));
+
+	
+
 
 	/// Draw the histogram and the fit
-	TCanvas* cPhiHist = new TCanvas("cPhiHist", "cPhiHist", 700, 600);
-	phiframe->Draw();
-	cPhiHist-> SaveAs(Form("Phi1D_lambCos%flambPhi%f.png", weights[chooseIdx][HXorCS][0], weights[chooseIdx][HXorCS][1]), "RECREATE");
+	TCanvas* cPhiHist = new TCanvas("cPhiHist", "cPhiHist", 600, 600);
 
+	TPad *padPhi1 = new TPad("padPhi1", "padPhi1", 0, 0.25, 1, 1.0);
+	padPhi1 -> SetBottomMargin(0.03);
+	padPhi1 -> SetTicks(1, 1);
+	padPhi1 -> Draw();
+	padPhi1 -> cd();
+	
+	// add legends
+	phiframe -> addObject(PolarParamsText(weights[chooseIdx][HXorCS][0], weights[chooseIdx][HXorCS][1], lambTheta, lambPhi, true));
+	phiframe -> GetXaxis() -> SetLabelOffset(1); // to make it disappear under the pull distribution pad
+	phiframe -> SetTitle("");
+	phiframe -> Draw();
+	gPad -> RedrawAxis();
 
+	// Pull Distribution
+	cCosHist -> cd();
+	
+	TPad* padPhi2 = GetPadPullDistribution(phiframe, fitResultPhi->floatParsFinal().getSize());
+
+	cPhiHist -> cd();
+  	padPhi1 -> Draw();
+  	padPhi2 -> Draw();
+
+  	cPhiHist-> SaveAs(Form("Phi1D_lambCos%flambPhi%f.png", weights[chooseIdx][HXorCS][0], weights[chooseIdx][HXorCS][1]), "RECREATE");
 
 	cout << "--------------------------------------" << endl;
 	cout << "Done phi fit!" << endl;
