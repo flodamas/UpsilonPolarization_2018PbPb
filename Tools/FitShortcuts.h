4842df0f6190d6b782212416938a3e5a1907f15b
Saeahram <85007865+ashleyahram@users.noreply.github.com>
2023-08-22 22:35:10 +0200

    Removed drawFitGraph function due to conflict
    Removed drawFitGraph function due to conflict between two files (one is using drawFitGraph, and the other is not). I separately made a header file for the DrawFitGraph function.

--- Tools/FitShortcuts.h
+++ Tools/FitShortcuts.h
@@ -187,112 +187,47 @@ RooFitResult* SymDSCBGaussfit(RooRealVar* massVar, RooWorkspace* wspace, RooData
 }
 
 
-void drawFitGraph(RooRealVar* massVar, RooWorkspace* wspace, RooDataSet* massDataset, TString pdfName, RooFitResult* fitResult, Float_t massMin, Float_t massMax, Int_t nBins){
-	
+
+RooFitResult* Hypatiafit(RooRealVar* massVar, RooWorkspace* wspace, RooDataSet* massDataset, Float_t massMin, Float_t massMax){
 	using namespace RooFit;
-	
-	/// draw the fit to see if the fit is reasonable
-	string spdfName = pdfName.Data(); // convert TString to string
-	auto* canvas = new TCanvas(Form("canvas%s",spdfName.c_str()), Form("canvas%s",spdfName.c_str()), 600, 600);
-	TPad* pad1 = new TPad("pad1", "pad1", 0, 0.25, 1, 1.0);
-	pad1->SetBottomMargin(0.03);
-	pad1->Draw();
-	pad1->cd();
 
-	/// define frame for the invariant mass plot with a fit
-	RooPlot* frame = massVar -> frame(Title(" "), Range(massMin, massMax));
-	frame->GetXaxis()->SetLabelOffset(1); // to make it disappear under the pull distribution pad
-	// frame->SetYTitle(Form("Candidates / (%d MeV)", (int)1000 * (binMax - binMin) / nBins));
-	massDataset -> plotOn(frame, Name("data"), Binning(nBins), DrawOption("P0Z"));
+	// fit
+	RooRealVar mean("meanHypatia", "", 9.457, 9., 10.);
+	RooRealVar lambda("lambdaHypatia","lambda of hypatia PDF",-1.0, -20.0, -0.1);
+	RooRealVar zeta("zetaHypatia","zeta of hypatia PDF",0.01, 0.0, 1.0);
+	RooRealVar beta("betaHypatia","beta of hypatia PDF",-0.01, -20.0, 0.0);
+	RooRealVar sigma("sigmaHypathia","sigma of hypatia PDF",0.15, 0.1, 0.3);
+	RooRealVar alphaInf("alphaInfHypatia","al1s of hypatia PDF", 3.0 , 0.1, 5.0);
+	RooRealVar alphaSup("alphaSupHypatia","ar1s of hypatia PDF", 3.0 , 0.1, 10.0);
+	RooRealVar orderInf("orderInfHypatia","nl1s of hypatia PDF", 1.0 , 0.2, 14.718);
+	RooRealVar orderSup("orderSupHypatia","nr1s of hypatia PDF", 1.0 , 0.2, 14.718);
 
-	RooAbsPdf* signal = nullptr;
-	/// get the signal pdf depending on the pdf model
-	if(pdfName=="SymDSCB") signal = wspace -> pdf("SymDSCB"); // symmetric core Double-sided Crystal Ball
-	else if(pdfName=="SymDSCBGauss") signal = wspace -> pdf("DSCBGauss"); // Double-sided Crystal Ball + Gaussian
-	else if(pdfName=="AsymDSCB") signal = wspace -> pdf("AsymDSCB"); // asymmetric core Double-sided Crystal Ball
-	else( "Error! can't find the pdf model..");
+	lambda.setVal(-1.76); 
+	// zeta.setVal(0); 
+	beta.setVal(-1.44); 
+	sigma.setVal(0.157); 
+	alphaInf.setVal(1.85); 
+	alphaSup.setVal(3.36); 
+	orderInf.setVal(1.04); 
+	orderSup.setVal(0.432); 
 
-	wspace -> Print();	
+	zeta.setConstant(1);
 
-	/// put the signal pdf on the frame
-	signal -> plotOn(frame, LineColor(kBlue));
+	RooHypatia2 signal("Hypatia","Hypatia", *massVar, lambda, zeta, beta, sigma, mean, alphaInf, orderInf, alphaSup, orderSup);
 
-	/// convert RooRealVals in wspace to Int_t
-	RooRealVar* centMinVar = wspace->var("centMinVar");
-	Int_t centMin = centMinVar -> getVal();
-	RooRealVar* centMaxVar = wspace->var("centMaxVar");
-	Int_t centMax = centMaxVar -> getVal();
-	
-	RooRealVar* ptMinVar = wspace->var("ptMinVar");
-	Int_t ptMin = ptMinVar -> getVal();
-	RooRealVar* ptMaxVar = wspace->var("ptMaxVar");
-	Int_t ptMax = ptMaxVar -> getVal();
-	
-	RooRealVar* cosThetaMinVar = wspace->var("cosThetaMinVar");
-	Int_t cosThetaMin = cosThetaMinVar -> getVal();
-	RooRealVar* cosThetaMaxVar = wspace->var("cosThetaMaxVar");
-	Int_t cosThetaMax = cosThetaMaxVar -> getVal();
+	cout << endl
+	     << "Fitting the MC signal shape (weighted entries!!) with a Hypatia PDF" << endl;
 
-	RooRealVar* isCSframeVar = wspace->var("isCSframeVar");
-	Int_t isCSframe = isCSframeVar -> getVal();
+	bool doWeightedError = true;
 
-	RooRealVar* phiMinVar = wspace->var("phiMinVar");
-	Int_t phiMin = phiMinVar -> getVal();
-	RooRealVar* phiMaxVar = wspace->var("phiMaxVar");
-	Int_t phiMax = phiMaxVar -> getVal();
+	auto* fitResult = signal.fitTo(*massDataset, Save(), Extended(true)/*, PrintLevel(-1)*/, Minos(!doWeightedError), NumCPU(3), Range(massMin, massMax), AsymptoticError(doWeightedError)); 
+	// quoting RooFit: "sum-of-weights and asymptotic error correction do not work with MINOS errors", so let's turn off Minos, no need to estimate asymmetric errors with MC fit
+	wspace -> import(signal);
 
-	/// Get variables in the signal
-	RooArgSet* params = nullptr;
-	params = signal -> getVariables() ;
-	// cout << "params of " << pdfName << ": " << *params << endl;
+	fitResult -> Print("v");
 
-	/// texts on the plot
-	frame->addObject(KinematicsText(centMin, centMax, ptMin, ptMax));
-	frame->addObject(RefFrameText(isCSframe, cosThetaMin, cosThetaMax, phiMin, phiMax));
-	if(pdfName=="SymDSCB"){
-		RooRealVar* meanVar = (RooRealVar*) params->find("meanSymDSCB");
-		RooRealVar* sigmaVar = (RooRealVar*) params->find("sigmaSymDSCB");
-		RooRealVar* alphaInfVar = (RooRealVar*) params->find("alphaInfSymDSCB");
-		RooRealVar* orderInfVar = (RooRealVar*) params->find("orderInfSymDSCB");
-		RooRealVar* alphaSupVar = (RooRealVar*) params->find("alphaSupSymDSCB");
-		RooRealVar* orderSupVar = (RooRealVar*) params->find("orderSupSymDSCB");
-		frame->addObject(SymCoreDoubleCBParamsText(*meanVar, *sigmaVar, *alphaInfVar, *orderInfVar, *alphaSupVar, *orderSupVar));
-	}
-	else if(pdfName=="SymDSCBGauss"){
-		RooRealVar* meanVar = (RooRealVar*) params->find("meanDSCBGauss");
-		RooRealVar* sigmaVar = (RooRealVar*) params->find("sigmaDSCBGauss");
-		RooRealVar* alphaInfVar = (RooRealVar*) params->find("alphaInfDSCBGauss");
-		RooRealVar* orderInfVar = (RooRealVar*) params->find("orderInfDSCBGauss");
-		RooRealVar* alphaSupVar = (RooRealVar*) params->find("alphaSupDSCBGauss");
-		RooRealVar* orderSupVar = (RooRealVar*) params->find("orderSupDSCBGauss");
-		RooRealVar* sigma_gaussVar = (RooRealVar*) params->find("sigma_gauss");
-		RooRealVar* normFractionVar = (RooRealVar*) params->find("normFraction");
-		frame->addObject(SymCoreDoubleCBGaussParamsText(*meanVar, *sigmaVar, *alphaInfVar, *orderInfVar, *alphaSupVar, *orderSupVar, *sigma_gaussVar, *normFractionVar));
-	}
-	else if(pdfName=="AsymDSCB"){
-		RooRealVar* meanVar = (RooRealVar*) params->find("meanAsymDSCB");
-		RooRealVar* sigmaInfVar = (RooRealVar*) params->find("sigmaInfAsymDSCB");
-		RooRealVar* alphaInfVar = (RooRealVar*) params->find("alphaInfAsymDSCB");
-		RooRealVar* orderInfVar = (RooRealVar*) params->find("orderInfAsymDSCB");
-		RooRealVar* sigmaSupVar = (RooRealVar*) params->find("sigmaSupAsymDSCB");
-		RooRealVar* alphaSupVar = (RooRealVar*) params->find("alphaSupAsymDSCB");
-		RooRealVar* orderSupVar = (RooRealVar*) params->find("orderSupAsymDSCB");
-		frame->addObject(AsymDoubleCBParamsText(*meanVar, *sigmaInfVar, *alphaInfVar, *orderInfVar, *sigmaSupVar, *alphaSupVar, *orderSupVar));	
-	}
-	else("Error! can't find the pdf model..");
-
-	frame->Draw();
-
-	canvas->cd();
-
-	TPad* pad2 = GetPadPullDistribution(frame, fitResult->floatParsFinal().getSize());
-
-	canvas->cd();
-	pad1->Draw();
-	pad2->Draw();
-	canvas->SaveAs(Form("SignalParameters/MCfit_%s_%dto%d.png", spdfName.c_str(), ptMin, ptMax), "RECREATE");
+ 	return fitResult;
 }
-
 
 RooArgSet GetMCSignalTailParameters(RooRealVar* alphaInf, RooRealVar* orderInf, RooRealVar* alphaSup, RooRealVar* orderSup, TString signalShapeName = "symCoreDSCB", Int_t centMin = 0, Int_t centMax = 90, Int_t ptMin = 0, Int_t ptMax = 30) {
 	RooArgSet tailParams(*alphaInf, *orderInf, *alphaSup, *orderSup);
