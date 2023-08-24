d9a2911d2ade2b23d2879bc5cd555615e4d809d9
ashleyahram <ashleyahram@naver.com>
2023-08-24 11:50:01 +0200

    Added GetMCSignalParameters function
    Added the GetMCSignalParameters function to obtain signal parameters not only tail parameters from MC fit

--- Tools/FitShortcuts.h
+++ Tools/FitShortcuts.h
@@ -256,3 +256,31 @@ RooArgSet GetMCSignalTailParameters(RooRealVar* alphaInf, RooRealVar* orderInf, 
 
 	return tailParams;
 }
+
+RooArgSet GetMCSignalParameters(RooRealVar* alphaInf, RooRealVar* orderInf, RooRealVar* alphaSup, RooRealVar* orderSup, TString signalShapeName = "symCoreDSCB", Int_t centMin = 0, Int_t centMax = 90, Int_t ptMin = 0, Int_t ptMax = 30) {
+	RooArgSet tailParams(*alphaInf, *orderInf, *alphaSup, *orderSup);
+
+	// if the .txt file for this specific fit model exists, just read the tail parameters from it
+	const char* mcFileName = Form("../MonteCarlo/SignalParameters/%s_cent%dto%d_pt%dto%d.txt", signalShapeName.Data(), centMin, centMax, ptMin, ptMax);
+
+	if (fopen(mcFileName, "r")) {
+		cout << endl
+		     << "Found " << mcFileName << " file, will read the signal tail parameters from it" << endl;
+		tailParams.readFromFile(mcFileName);
+	} else {
+		cout << endl
+		     << mcFileName << " file does not seem to exist, you need to extract the signal tail paramaters from MC fit first!" << endl;
+	}
+
+	// fix the tail parameters
+	alphaInf->setConstant();
+	orderInf->setConstant();
+	alphaSup->setConstant();
+	orderSup->setConstant();
+
+	cout << endl
+	     << "Tail parameters fixed to the following MC signal values:" << endl;
+	tailParams.Print("v");
+
+	return tailParams;
+}
