136e48c05d702e02a4128ae942b61dc49312a6e3
Florian Damas <florian.damas@cern.ch>
2023-11-22 15:15:24 +0100

    Improve code for (cos theta, phi) map plotting

--- Sandbox/drawCosThetaPhiMapWeightedDataset.C
+++ Sandbox/drawCosThetaPhiMapWeightedDataset.C
@@ -1,11 +1,26 @@
 #include "../Tools/BasicHeaders.h"
 
-// #include "../Tools/Style/tdrStyle.C"
-// #include "../Tools/Style/CMS_lumi.C"
-#include "../Tools/FitShortcuts.h"
+#include "../AnalysisParameters.h"
 
-void drawCosThetaPhiMapWeightedDataset(Int_t minPt = 0, Int_t maxPt = 30, Int_t centMin = 0, Int_t centMax = 90,
-						const char* filename = "../Files/WeightedUpsilonSkimmedDataset.root") {
+// reduce the whole dataset (N dimensions) to 2D (cos theta, phi)
+RooDataSet* CosThetaPhiWeightedDataset(RooDataSet* allDataset, RooWorkspace* wspace, Bool_t isCSframe = kTRUE, Int_t ptMin = 0, Int_t ptMax = 30, Double_t massMin = 8, Double_t massMax = 14) {
+	if (allDataset == nullptr) {
+		cerr << "Null RooDataSet provided to the reducer method!!" << endl;
+		return nullptr;
+	}
+
+	const char* kinematicCut = Form("(centrality >= %d && centrality < %d) && (mass > %f && mass < %f) && (rapidity > %f && rapidity < %f) && (pt > %d && pt < %d)", 2 * gCentralityBinMin, 2 * gCentralityBinMax, massMin, massMax, gRapidityMin, gRapidityMax, ptMin, ptMax);
+
+	RooDataSet* reducedDataset = (RooDataSet*)allDataset->reduce(RooArgSet(*(wspace->var(isCSframe ? "cosThetaCS" : "cosThetaHX")), *(wspace->var(isCSframe ? "phiCS" : "phiHX"))), kinematicCut);
+
+	reducedDataset->SetName(kinematicCut); // just to make it unique
+
+	//	wspace->import(*reducedDataset);
+
+	return reducedDataset;
+}
+
+void drawCosThetaPhiMapWeightedDataset(Int_t minPt = 0, Int_t maxPt = 30, const char* filename = "../Files/WeightedUpsilonSkimmedDataset.root") {
 	TFile* f = TFile::Open(filename, "READ");
 	if (!f) {
 		cout << "File " << filename << " not found. Check the directory of the file." << endl;
@@ -14,12 +29,12 @@ void drawCosThetaPhiMapWeightedDataset(Int_t minPt = 0, Int_t maxPt = 30, Int_t 
 
 	cout << "File " << filename << " opened" << endl;
 
-	writeExtraText = false; // if extra text
-	// extraText = "       Internal";
+	writeExtraText = true; // if extra text
+	extraText = "       Internal";
 
 	// (cos theta, phi) 2D distribution maps for CS and HX frames
 
-	Double_t massMin = 8., massMax = 14.;
+	Double_t massMin = 9., massMax = 11.;
 
 	Int_t nCosThetaBins = 20;
 	Float_t cosThetaMin = -1, cosThetaMax = 1;
@@ -48,59 +63,58 @@ void drawCosThetaPhiMapWeightedDataset(Int_t minPt = 0, Int_t maxPt = 30, Int_t 
 	wspace->Print();
 
 	/// Reduce the dataset to an interested region (cent, pT cuts)
-	RooDataSet* reducedDatasetCS = ReducedWeightedDatasetCS(allDatasetCS, wspace, minPt, maxPt, massMin, massMax, cosThetaMin, cosThetaMax, phiMin, phiMax);
-	RooDataSet* reducedDatasetHX = ReducedWeightedDatasetHX(allDatasetHX, wspace, minPt, maxPt, massMin, massMax, cosThetaMin, cosThetaMax, phiMin, phiMax);
-	
-	/// read variables in the reduced dataset in the workspace
-	RooRealVar* centrality = wspace -> var("centrality");
-	RooRealVar* mass = wspace -> var("mass");
-	RooRealVar* pt = wspace -> var("pt");
-	RooRealVar* cosThetaCS = wspace -> var("cosThetaCS");
-	RooRealVar* phiCS = wspace -> var("phiCS");
-	RooRealVar* cosThetaHX = wspace -> var("cosThetaHX");
-	RooRealVar* phiHX = wspace -> var("phiHX");
+	auto* cosThetaPhiDatasetCS = CosThetaPhiWeightedDataset(allDatasetCS, wspace, true, minPt, maxPt, massMin, massMax);
+	auto* cosThetaPhiDatasetHX = CosThetaPhiWeightedDataset(allDatasetHX, wspace, false, minPt, maxPt, massMin, massMax);
 
-	// Long64_t nEntries = reducedDataset -> sumEntries();
+	/// read variables in the reduced dataset in the workspace
+	RooRealVar* cosThetaCS = wspace->var("cosThetaCS");
+	RooRealVar* phiCS = wspace->var("phiCS");
+	RooRealVar* cosThetaHX = wspace->var("cosThetaHX");
+	RooRealVar* phiHX = wspace->var("phiHX");
 
 	/// Set the plot style
 	gStyle->SetPadLeftMargin(.15);
-	gStyle->SetTitleYOffset(1.2);	
+	gStyle->SetTitleYOffset(1.2);
 	gStyle->SetPadRightMargin(0.2);
 	gStyle->SetTitleOffset(1.4, "z");
 	gStyle->SetPalette(kRainBow);
+	gStyle->SetNumberContours(256);
 
 	/// Defind legend
 	TLatex* legend = new TLatex();
 	legend->SetTextAlign(22);
 	legend->SetTextSize(0.05);
 
-	/// Draw and save the number of events(signal+background) plots in the 2D (costheta, phi) space in CS 
-	TCanvas* cCS = new TCanvas("cCS", "cCS", 650, 600);
-	TH2* hCS= dynamic_cast<TH2*>(reducedDatasetCS->createHistogram("hCS", *cosThetaCS, Binning(nCosThetaBins,cosThetaMin,cosThetaMax), YVar(*phiCS, Binning(nPhiBins,phiMin,phiMax))));
-	hCS -> SetTitle(";cos #theta_{CS}; #varphi_{CS} (#circ)");
-	hCS -> Draw("COLZ");
+	/// Draw and save the number of events(signal+background) plots in the 2D (costheta, phi) space in CS
+	TCanvas* cCS = new TCanvas("cCS", "cCS", 700, 600);
+	TH2* hCS = dynamic_cast<TH2*>(cosThetaPhiDatasetCS->createHistogram("hCS", *cosThetaCS, Binning(nCosThetaBins, cosThetaMin, cosThetaMax), YVar(*phiCS, Binning(nPhiBins, phiMin, phiMax))));
 
-	legend -> DrawLatexNDC(.48, .88, Form("centrality %d-%d%%, %d < p_{T}^{#mu#mu} < %d GeV", centMin, centMax, minPt, maxPt));
-	legend -> DrawLatexNDC(.48, .8, Form("%d < m_{#mu#mu} < %d GeV", 8, 14));
+	hCS->SetTitle(";cos #theta_{CS}; #varphi_{CS} (#circ)");
+	hCS->Draw("COLZ");
 
-	hCS -> GetYaxis() -> SetRangeUser(-190, 300);
-	gPad -> Update();
-	CMS_lumi(cCS, "2018 PbPb miniAOD, DoubleMuon PD");
-	cCS -> SaveAs(Form("frame_distrib/WeightedCS_cent%dto%d_pt%dto%dGeV.png", centMin, centMax, minPt, maxPt), "RECREATE");
+	legend->DrawLatexNDC(.48, .88, Form("centrality %d-%d%%, %d < p_{T}^{#mu#mu} < %d GeV/c", gCentralityBinMin, gCentralityBinMax, minPt, maxPt));
+	legend->DrawLatexNDC(.48, .8, Form("%.0f < m_{#mu#mu} < %.0f GeV/c^{2}", massMin, massMax));
 
-	/// Draw and save the number of events(signal+background) plots in the 2D (costheta, phi) space in HX 
-	TCanvas* cHX = new TCanvas("cHX", "cHX", 650, 600);
-	TH2* hHX= dynamic_cast<TH2*>(reducedDatasetHX->createHistogram("hHX", *cosThetaHX, Binning(nCosThetaBins,cosThetaMin,cosThetaMax), YVar(*phiHX, Binning(nPhiBins,phiMin,phiMax))));
-	hHX -> SetTitle(";cos #theta_{HX}; #varphi_{HX} (#circ)");
-	hHX -> Draw("COLZ");
+	hCS->GetYaxis()->SetRangeUser(-190, 300);
+	gPad->Update();
 
-	legend -> DrawLatexNDC(.48, .88, Form("centrality %d-%d%%, %d < p_{T}^{#mu#mu} < %d GeV", centMin, centMax, minPt, maxPt));
-	legend -> DrawLatexNDC(.48, .8, Form("%d < m_{#mu#mu} < %d GeV", 8, 14));
+	CMS_lumi(cCS, gCMSLumiText);
+	cCS->SaveAs(Form("frame_distrib/WeightedCS_cent%dto%d_pt%dto%dGeV.png", gCentralityBinMin, gCentralityBinMax, minPt, maxPt), "RECREATE");
 
-	hHX -> GetYaxis() -> SetRangeUser(-190, 300);
-	gPad -> Update();
-	CMS_lumi(cHX, "2018 PbPb miniAOD, DoubleMuon PD");
-	cHX -> SaveAs(Form("frame_distrib/WeightedHX_cent%dto%d_pt%dto%dGeV.png", centMin, centMax, minPt, maxPt), "RECREATE");
+	/// Draw and save the number of events(signal+background) plots in the 2D (costheta, phi) space in HX
+	TCanvas* cHX = new TCanvas("cHX", "cHX", 700, 600);
+	TH2* hHX = dynamic_cast<TH2*>(cosThetaPhiDatasetHX->createHistogram("hHX", *cosThetaHX, Binning(nCosThetaBins, cosThetaMin, cosThetaMax), YVar(*phiHX, Binning(nPhiBins, phiMin, phiMax))));
+	hHX->SetTitle(";cos #theta_{HX}; #varphi_{HX} (#circ)");
+	hHX->Draw("COLZ");
+
+	legend->DrawLatexNDC(.48, .88, Form("centrality %d-%d%%, %d < p_{T}^{#mu#mu} < %d GeV/c", gCentralityBinMin, gCentralityBinMax, minPt, maxPt));
+	legend->DrawLatexNDC(.48, .8, Form("%.0f < m_{#mu#mu} < %.0f GeV/c^{2}", massMin, massMax));
+
+	hHX->GetYaxis()->SetRangeUser(-190, 300);
+	gPad->Update();
+
+	CMS_lumi(cHX, gCMSLumiText);
+	cHX->SaveAs(Form("frame_distrib/WeightedHX_cent%dto%d_pt%dto%dGeV.png", gCentralityBinMin, gCentralityBinMax, minPt, maxPt), "RECREATE");
 
 	/// save the results in a file for later usage
 	const char* outputFileName = Form("frame_distrib/WeightedCosThetaPhiMap_pt%dto%dGeV.root", minPt, maxPt);
@@ -113,15 +127,10 @@ void drawCosThetaPhiMapWeightedDataset(Int_t minPt = 0, Int_t maxPt = 30, Int_t 
 
 	cout << endl
 	     << "CosTheta-Phi maps saved in " << outputFileName << endl;
-
 }
 
-void scandrawCosThetaPhiMapWeightedDataset(){
-
-	Int_t PtEdges[9] = {0, 2, 4, 6, 8, 12, 16, 20, 30};
-	Int_t NumPtEle = sizeof(PtEdges)/sizeof(Int_t);
-	for(Int_t idx =0; idx < NumPtEle-1; idx++){
-		drawCosThetaPhiMapWeightedDataset(PtEdges[idx], PtEdges[idx+1], 0, 90, "../Files/WeightedUpsilonSkimmedDataset.root");
+void scandrawCosThetaPhiMapWeightedDataset() {
+	for (Int_t idx = 0; idx < NPtBins; idx++) {
+		drawCosThetaPhiMapWeightedDataset(gPtBinning[idx], gPtBinning[idx + 1], "../Files/WeightedUpsilonSkimmedDataset.root");
 	}
 }
-
