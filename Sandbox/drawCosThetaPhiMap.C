dae3de098857810cda2cb93d487dda680c706987
ashleyahram <ashleyahram@maver.com>
2024-02-07 11:15:07 +0100

    changed the format of the code to be the same as drawCosThetaPhiMapWeightedDataset.C

--- Sandbox/drawCosThetaPhiMap.C
+++ Sandbox/drawCosThetaPhiMap.C
@@ -1,9 +1,56 @@
-#include "../Tools/Style/tdrStyle.C"
-#include "../Tools/Style/CMS_lumi.C"
-#include "../Tools/FitShortcuts.h"
+#include "../Tools/BasicHeaders.h"
 
-void drawCosThetaPhiMap(Int_t minPt = 0, Int_t maxPt = 30, Int_t centMin = 0, Int_t centMax = 90,
-						const char* filename = "../Files/upsilonSkimmedDataset.root") {
+#include "../AnalysisParameters.h"
+
+TH2* DrawCosThetaPhiDistribution(RooDataSet* dataset, RooWorkspace& wspace, const char* frameAcronym = "CS", Int_t ptMin = 0, Int_t ptMax = 30) {
+	Double_t massMin = 9., massMax = 11.;
+
+	Int_t nCosThetaBins = 20;
+	Float_t cosThetaMin = -1, cosThetaMax = 1;
+
+	Int_t nPhiBins = 11;
+	Float_t phiMin = 0, phiMax = 220;
+
+	const char* histoName = Form("%s_cent%dto%d_pt%dto%dGeV", frameAcronym, gCentralityBinMin, gCentralityBinMax, ptMin, ptMax);
+
+	wspace.import(*dataset);
+	RooRealVar cosThetaVar = *wspace.var(Form("cosTheta%s", frameAcronym));
+	RooRealVar phiVar = *wspace.var(Form("phi%s", frameAcronym));
+
+	// reduce the whole dataset (N dimensions) to 2D (cos theta, phi)
+	const char* kinematicCut = Form("(centrality >= %d && centrality < %d) && (mass > %f && mass < %f) && (rapidity > %f && rapidity < %f) && (pt > %d && pt < %d)", 2 * gCentralityBinMin, 2 * gCentralityBinMax, massMin, massMax, gRapidityMin, gRapidityMax, ptMin, ptMax);
+
+	RooDataSet* reducedDataset = (RooDataSet*)dataset->reduce(RooArgSet(cosThetaVar, phiVar), kinematicCut);
+
+	/// Draw and save the number of events(signal+background) plots in the 2D (costheta, phi) space
+
+	TCanvas* canvas = new TCanvas(Form("canvas%s", frameAcronym), "canvas", 700, 600);
+
+	TH2* histo = dynamic_cast<TH2*>(reducedDataset->createHistogram(histoName, cosThetaVar, RooFit::Binning(nCosThetaBins, cosThetaMin, cosThetaMax), RooFit::YVar(phiVar, RooFit::Binning(nPhiBins, phiMin, phiMax))));
+
+	histo->SetTitle(Form(";cos #theta_{%s}; |#varphi_{%s}| (#circ)", frameAcronym, frameAcronym));
+	histo->Draw("COLZ");
+
+	TLatex legend;
+	legend.SetTextAlign(22);
+	legend.SetTextSize(0.05);
+	legend.DrawLatexNDC(.48, .86, Form("centrality %d-%d%%, %d < p_{T}^{#mu#mu} < %d GeV/c", gCentralityBinMin, gCentralityBinMax, ptMin, ptMax));
+	legend.DrawLatexNDC(.48, .78, Form("%.0f < m_{#mu#mu} < %.0f GeV/c^{2}", massMin, massMax));
+
+	histo->GetXaxis()->CenterTitle();
+	histo->GetYaxis()->SetRangeUser(0, 250);
+	histo->GetYaxis()->CenterTitle();
+	histo->GetZaxis()->SetMaxDigits(3);
+
+	gPad->Update();
+
+	CMS_lumi(canvas, gCMSLumiText);
+	canvas->SaveAs(Form("frame_distrib/%s.png", histoName), "RECREATE");
+
+	return histo;
+}
+
+void drawCosThetaPhiMap(Int_t ptMin = 0, Int_t ptMax = 30, const char* filename = "../Files/UpsilonSkimmedDataset.root") {
 	TFile* f = TFile::Open(filename, "READ");
 	if (!f) {
 		cout << "File " << filename << " not found. Check the directory of the file." << endl;
@@ -12,96 +59,41 @@ void drawCosThetaPhiMap(Int_t minPt = 0, Int_t maxPt = 30, Int_t centMin = 0, In
 
 	cout << "File " << filename << " opened" << endl;
 
-	writeExtraText = false; // if extra text
-	// extraText = "       Internal";
+	writeExtraText = true; // if extra text
+	extraText = "       Internal";
 
-	// (cos theta, phi) 2D distribution maps for CS and HX frames
-
-	Int_t nCosThetaBins = 21;
-	Float_t cosThetaMin = -1, cosThetaMax = 1;
-
-	//	Double_t cosThetaBinning[] = {-1, -0.8, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.8, 1};
-	//	nCosThetaBins = sizeof(cosThetaBinning) / sizeof(Double_t) - 1;
-
-	Int_t nPhiBins = 23;
-	Float_t phiMin = -180, phiMax = 280;
-
-	//Double_t phiBinning[] = {-TMath::Pi(), -2.5, -2, -1.5, -1, -0.5, 0.5, 1, 1.5, 2, 2.5, 180};
-	//	nPhiBins = sizeof(cosThetaBinning) / sizeof(Double_t) - 1;
+	gStyle->SetPadLeftMargin(.15);
+	gStyle->SetTitleYOffset(1.2);
+	gStyle->SetPadRightMargin(0.18);
+	gStyle->SetTitleOffset(1.2, "z");
+	gStyle->SetPalette(kRainBow);
+	gStyle->SetNumberContours(256);
 
 	using namespace RooFit;
 	RooMsgService::instance().setGlobalKillBelow(RooFit::WARNING);
 
-	/// Read skimmed dataset (contains angular distributions in CS and HX after kinematic cuts due to acceptance)
-	RooDataSet* allDataset = (RooDataSet*)f->Get("dataset");
-	/// import the dataset to a workspace
-	RooWorkspace* wspace = new RooWorkspace("workspace");
-	wspace->import(*allDataset);
+	/// Draw and save the number of events(signal+background) plots in the 2D (costheta, phi) space
+	RooWorkspace wspace("workspace");
 
-	/// Reduce the dataset to an interested region (cent, pT cuts)
-	RooDataSet* reducedDataset = ReducedDataset(allDataset, wspace, centMin, centMax, minPt, maxPt, kFALSE, cosThetaMin, cosThetaMax, phiMin, phiMax);
-	
-	/// read variables in the reduced dataset in the workspace
-	RooRealVar* centrality = wspace -> var("centrality");
-	RooRealVar* mass = wspace -> var("mass");
-	RooRealVar* pt = wspace -> var("pt");
-	RooRealVar* cosThetaCS = wspace -> var("cosThetaCS");
-	RooRealVar* phiCS = wspace -> var("phiCS");
-	RooRealVar* cosThetaHX = wspace -> var("cosThetaHX");
-	RooRealVar* phiHX = wspace -> var("phiHX");
+	TH2* histoCS = DrawCosThetaPhiDistribution((RooDataSet*)f->Get("dataset"), wspace, "CS", ptMin, ptMax);
 
-	// Long64_t nEntries = reducedDataset -> sumEntries();
-
-	/// Set the plot style
-	gStyle->SetPadLeftMargin(.15);
-	gStyle->SetTitleYOffset(1.1);	
-	gStyle->SetPadRightMargin(0.18);
-	gStyle->SetTitleOffset(1.4, "z");
-	gStyle->SetPalette(kRainBow);
-
-	/// Defind legend
-	TLatex* legend = new TLatex();
-	legend->SetTextAlign(22);
-	legend->SetTextSize(0.05);
-
-	/// Draw and save the number of events(signal+background) plots in the 2D (costheta, phi) space in CS 
-	TCanvas* cCS = new TCanvas("cCS", "cCS", 600, 600);
-	TH2* hCS= dynamic_cast<TH2*>(reducedDataset->createHistogram("hCS", *cosThetaCS, Binning(nCosThetaBins,cosThetaMin,cosThetaMax), YVar(*phiCS, Binning(nPhiBins,phiMin,phiMax))));
-	hCS -> SetTitle(";cos #theta_{CS}; #varphi_{CS} (#circ)");
-	hCS -> Draw("COLZ");
-
-	legend -> DrawLatexNDC(.48, .88, Form("centrality %d-%d%%, %d < p_{T}^{#mu#mu} < %d GeV", centMin, centMax, minPt, maxPt));
-	legend -> DrawLatexNDC(.48, .8, Form("%d < m_{#mu#mu} < %d GeV", 8, 14));
-
-	hCS -> GetYaxis() -> SetRangeUser(-190, 300);
-	gPad -> Update();
-	CMS_lumi(cCS, "2018 PbPb miniAOD, DoubleMuon PD");
-	cCS -> SaveAs(Form("frame_distrib/CS_cent%dto%d_pt%dto%dGeV.png", centMin, centMax, minPt, maxPt), "RECREATE");
-
-	/// Draw and save the number of events(signal+background) plots in the 2D (costheta, phi) space in HX 
-	TCanvas* cHX = new TCanvas("cHX", "cHX", 600, 600);
-	TH2* hHX= dynamic_cast<TH2*>(reducedDataset->createHistogram("hHX", *cosThetaHX, Binning(nCosThetaBins,cosThetaMin,cosThetaMax), YVar(*phiHX, Binning(nPhiBins,phiMin,phiMax))));
-	hHX -> SetTitle(";cos #theta_{HX}; #varphi_{HX} (#circ)");
-	hHX -> Draw("COLZ");
-
-	legend -> DrawLatexNDC(.48, .88, Form("centrality %d-%d%%, %d < p_{T}^{#mu#mu} < %d GeV", centMin, centMax, minPt, maxPt));
-	legend -> DrawLatexNDC(.48, .8, Form("%d < m_{#mu#mu} < %d GeV", 8, 14));
-
-	hHX -> GetYaxis() -> SetRangeUser(-190, 300);
-	gPad -> Update();
-	CMS_lumi(cHX, "2018 PbPb miniAOD, DoubleMuon PD");
-	cHX -> SaveAs(Form("frame_distrib/HX_cent%dto%d_pt%dto%dGeV.png", centMin, centMax, minPt, maxPt), "RECREATE");
+	TH2* histoHX = DrawCosThetaPhiDistribution((RooDataSet*)f->Get("dataset"), wspace, "HX", ptMin, ptMax);
 
 	/// save the results in a file for later usage
-	const char* outputFileName = Form("frame_distrib/CosThetaPhiMap_pt%dto%dGeV.root", minPt, maxPt);
-	TFile outputFile(outputFileName, "RECREATE");
+	gSystem->mkdir("frame_distrib", kTRUE);
+	TFile outputFile("frame_distrib/CosThetaPhiMaps.root", "UPDATE");
 
-	hHX->Write();
-	hCS->Write();
+	histoHX->Write();
+	histoCS->Write();
 
 	outputFile.Close();
 
 	cout << endl
-	     << "CosTheta-Phi maps saved in " << outputFileName << endl;
+	     << "CosTheta-Phi maps saved in " << outputFile.GetName() << endl;
+}
 
+void scanDrawCosThetaPhiMap(const char* filename = "../Files/UpsilonSkimmedDataset.root") {
+	for (Int_t idx = 0; idx < NPtBins; idx++) {
+		drawCosThetaPhiMap(gPtBinning[idx], gPtBinning[idx + 1], filename);
+	}
 }
