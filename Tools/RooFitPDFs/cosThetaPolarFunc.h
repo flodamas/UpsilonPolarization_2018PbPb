4fb115c769ca177fd0549bb28d08d5a61db7f75f
ashleyahram <ashleyahram@maver.com>
2024-05-27 19:04:23 -0700

    released the limits of the polarization fit to -2 to 2

--- Tools/RooFitPDFs/cosThetaPolarFunc.h
+++ Tools/RooFitPDFs/cosThetaPolarFunc.h
@@ -5,7 +5,7 @@ TF1* cosThetaPolarFunc(Float_t maxYield){
 	
 	cosThetaPolarFunc->SetParameter(0, 0);
 	cosThetaPolarFunc->SetParameter(1, 0.5 * maxYield);
-	cosThetaPolarFunc->SetParLimits(0, -1, 1);
+	cosThetaPolarFunc->SetParLimits(0, -2, 2);
 
 	cosThetaPolarFunc->SetParName(0, "lambdaTheta");
 	cosThetaPolarFunc->SetParName(1, "normFactor");
