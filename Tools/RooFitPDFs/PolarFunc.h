f63ca8b4d49c473d3c5389521119de085f5b83e1
ashleyahram <ashleyahram@maver.com>
2024-06-24 17:38:25 -0700

    minor change: change the function name

--- Tools/RooFitPDFs/PolarFunc.h
+++ Tools/RooFitPDFs/PolarFunc.h
@@ -1,6 +1,6 @@
 #include "../BasicHeaders.h"
 
-TF1* cosThetaPolarFunc(Float_t maxYield) {
+TF1* getCosThetaPolarFunc(Float_t maxYield) {
 	TF1* cosThetaPolarFunc = new TF1("PolarFunc", "[0] / (3 + [1]) * (1 + [1] * x * x)", -1, 1);
 
 	cosThetaPolarFunc->SetParameter(0, 0.5 * maxYield);
@@ -17,7 +17,7 @@ TF1* cosThetaPolarFunc(Float_t maxYield) {
 	return cosThetaPolarFunc;
 }
 
-TF1* phiPolarFunc(Float_t maxYield) {
+TF1* getPhiPolarFunc(Float_t maxYield) {
 	TF1* phiPolarFunc = new TF1("phiPolarFunc", "[0] * (1. + 2. * [2] / (3. + [1]) * TMath::Cos(2. * x * pi / 180.))", -180, 180);
 
 	phiPolarFunc->SetParameter(0, 0.5 * maxYield);
@@ -37,25 +37,25 @@ TF1* phiPolarFunc(Float_t maxYield) {
 	return phiPolarFunc;
 }
 
-TF2* generalPolarFunc(Float_t maxYield) {
-	TF2* polarFunc2D = new TF2("polarFunc2D", "[0] / (3 + [1]) * (1 + [1] * x * x + [2] * (1 - x * x) * TMath::Cos(2. * y * pi / 180.) + [3] * TMath::Sqrt(1 - x * x) * x * TMath::Cos(y * pi / 180.))", -1, 1, -180, 180);
+TF2* getGeneralPolarFunc(Float_t maxYield) {
+	TF2* generalPolarFunc = new TF2("generalPolarFunc", "[0] / (3 + [1]) * (1 + [1] * x * x + [2] * (1 - x * x) * TMath::Cos(2. * y * pi / 180.) + [3] * TMath::Sqrt(1 - x * x) * x * TMath::Cos(y * pi / 180.))", -1, 1, -180, 180);
 
-	polarFunc2D->SetParameter(0, 0.5 * maxYield);
-	polarFunc2D->SetParameter(1, 0);
-	polarFunc2D->SetParameter(2, 0);
-	polarFunc2D->SetParameter(3, 0);
+	generalPolarFunc->SetParameter(0, 0.5 * maxYield);
+	generalPolarFunc->SetParameter(1, 0);
+	generalPolarFunc->SetParameter(2, 0);
+	generalPolarFunc->SetParameter(3, 0);
  
-	polarFunc2D->SetParLimits(1, -2, 2);
-	polarFunc2D->SetParLimits(2, -2, 2);
-	polarFunc2D->SetParLimits(3, -2, 2);
+	generalPolarFunc->SetParLimits(1, -2, 2);
+	generalPolarFunc->SetParLimits(2, -2, 2);
+	generalPolarFunc->SetParLimits(3, -2, 2);
 
-	polarFunc2D->SetParName(0, "normFactor");
-	polarFunc2D->SetParName(1, "lambdaTheta");
-	polarFunc2D->SetParName(2, "lambdaPhi");
-	polarFunc2D->SetParName(3, "lambdaThetaPhi");
+	generalPolarFunc->SetParName(0, "normFactor");
+	generalPolarFunc->SetParName(1, "lambdaTheta");
+	generalPolarFunc->SetParName(2, "lambdaPhi");
+	generalPolarFunc->SetParName(3, "lambdaThetaPhi");
 
-	polarFunc2D->SetLineWidth(3);
-	polarFunc2D->SetLineColor(kRed + 1);
+	generalPolarFunc->SetLineWidth(3);
+	generalPolarFunc->SetLineColor(kRed + 1);
 
-	return polarFunc2D;
+	return generalPolarFunc;
 }
