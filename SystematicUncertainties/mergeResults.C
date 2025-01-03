c0efe61939cafc9887e08824685fe8d81de260e1
ashleyahram <srmyoo@ucdavis.edu>
2025-01-03 15:01:47 +0100

    fix some bugs

--- SystematicUncertainties/mergeResults.C
+++ SystematicUncertainties/mergeResults.C
@@ -1,12 +1,20 @@
 /// Merge the result histograms from several files into a single file
 
-void mergeResults(Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = kFALSE, Float_t cosThetaMin = -0.42, Float_t cosThetaMax = -0.14, Int_t phiMin = 60, Int_t phiMax = 120, Long64_t nPseudoExperiments = 100) {
+#include "../Tools/BasicHeaders.h"
+
+#include "../AnalysisParameters.h"
+
+#include "../Tools/FitShortcuts.h"
+
+#include "../MonteCarlo/AccEffHelpers.h"
+
+void mergeResults(Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = kFALSE, Float_t cosThetaMin = -0.70, Float_t cosThetaMax = -0.42, Int_t phiMin = 0, Int_t phiMax = 60, Long64_t nPseudoExperiments = 100, const char* altSignalShapeName = "SymDSCB", const char* altBkgShapeName = "ChebychevOrder3") {
     
     int nFiles = 1000;
 
-    const char* altSignalShapeName = "SymDSCB";
+    // const char* altSignalShapeName = "SymDSCB";
 
-    const char* altBkgShapeName = "ExpTimesErr";
+    // // const char* altBkgShapeName = "ExpTimesErr";
     // const char* altBkgShapeName = "ChebychevOrder3";
     
     const char* refFrameName = isCSframe ? "CS" : "HX";
@@ -20,7 +28,7 @@ void mergeResults(Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = kFALSE, F
         fileLocation = Form("YieldDifferencePlots/%s_%s_pt%dto%d_%s_cosTheta%.2fto%.2f_phi%dto%d_n%lld/", altSignalShapeName, altBkgShapeName, ptMin, ptMax, refFrameName, cosThetaMin, cosThetaMax, phiMin, phiMax, nPseudoExperiments);
         // cout << fileLocation << endl;
 
-        TString filename = Form("%soutput_%d.root", fileLocation, i);
+        TString filename = Form("%soutput_%d_cosTheta_%.2f_%.2f_phi_%.2f_%.2f.root", fileLocation, i, cosThetaMin, cosThetaMax, (double)phiMin, (double)phiMax);
         TFile *file = TFile::Open(filename, "READ");
 
         cout << filename << endl;
@@ -55,4 +63,25 @@ void mergeResults(Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = kFALSE, F
     outFile->Close();
     
     std::cout << "Histograms successfully merged into merged_output.root!" << std::endl;
+}
+
+void multipleMergeResults(Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = kFALSE, const char* signalShapeName = "SymDSCB", const char* bkgShapeName = "ExpTimesErr") {
+
+    Int_t nCosThetaBins = 5; 
+    Double_t cosThetaMin = -0.7; 
+    Double_t cosThetaMax = 0.7;
+
+    Int_t nPhiBins = 3;
+    Int_t phiMin = 0; 
+    Int_t phiMax = 180; 
+
+    std::vector<Double_t> cosThetaEdges = setCosThetaBinEdges(nCosThetaBins, cosThetaMin, cosThetaMax);
+
+	std::vector<Double_t> phiEdges = setPhiBinEdges(nPhiBins, phiMin, phiMax);
+
+    for (Int_t i = 0; i < nCosThetaBins; ++i) {
+        for (Int_t j = 0; j < nPhiBins; ++j) {
+            mergeResults(ptMin, ptMax, isCSframe, cosThetaEdges[i], cosThetaEdges[i+1], phiEdges[j], phiEdges[j+1], 100, signalShapeName, bkgShapeName);
+        }
+    }
 }
