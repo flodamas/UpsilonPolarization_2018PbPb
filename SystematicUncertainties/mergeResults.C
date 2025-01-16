1c9b3730a4a4d12fbb32008aa9848743316e2c28
ashleyahram <srmyoo@ucdavis.edu>
2025-01-16 09:13:07 +0100

    fix the error when 0 file doesn't exist

--- SystematicUncertainties/mergeResults.C
+++ SystematicUncertainties/mergeResults.C
@@ -8,8 +8,9 @@
 
 #include "../MonteCarlo/AccEffHelpers.h"
 
-void mergeResults(const char* altSignalShapeName = "SymDSCB", const char* altBkgShapeName = "ChebychevOrder3", Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = kFALSE, Float_t cosThetaMin = -0.70, Float_t cosThetaMax = -0.42, Int_t phiMin = 0, Int_t phiMax = 60, Long64_t nPseudoExperiments = 100) {
-    
+// void mergeResults(const char* altSignalShapeName = "SymDSCB", const char* altBkgShapeName = "ChebychevOrder3", Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = kFALSE, Float_t cosThetaMin = -0.70, Float_t cosThetaMax = -0.42, Int_t phiMin = 0, Int_t phiMax = 60, Long64_t nPseudoExperiments = 100) {
+void mergeResults(const char* altSignalShapeName = "Johnson", const char* altBkgShapeName = "ExpTimesErr", Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = kFALSE, Float_t cosThetaMin = -0.7, Float_t cosThetaMax = -0.42, Int_t phiMin = 0, Int_t phiMax = 60, Long64_t nPseudoExperiments = 100) { 
+
     int nFiles = 1000;
 
     // const char* altSignalShapeName = "SymDSCB";
@@ -23,9 +24,11 @@ void mergeResults(const char* altSignalShapeName = "SymDSCB", const char* altBkg
 
     char* fileLocation; 
 
+    int startIdx = 0;
+
     for (int i = 0; i < nFiles; ++i) {
 
-        fileLocation = Form("YieldDifferencePlots/%s_%s_pt%dto%d_%s_cosTheta%.2fto%.2f_phi%dto%d_n%lld/", altSignalShapeName, altBkgShapeName, ptMin, ptMax, refFrameName, cosThetaMin, cosThetaMax, phiMin, phiMax, nPseudoExperiments);
+        fileLocation = Form("YieldDifferencePlots_mass7to11p5_final/%s_%s_pt%dto%d_%s_cosTheta%.2fto%.2f_phi%dto%d_n%lld/", altSignalShapeName, altBkgShapeName, ptMin, ptMax, refFrameName, cosThetaMin, cosThetaMax, phiMin, phiMax, nPseudoExperiments);
         // cout << fileLocation << endl;
 
         TString filename = Form("%soutput_%d.root", fileLocation, i);
@@ -37,12 +40,13 @@ void mergeResults(const char* altSignalShapeName = "SymDSCB", const char* altBkg
         if (!file || file->IsZombie()) {
             std::cerr << "Error: Failed to open file" << filename << "!" << std::endl;
             if (file) file->Close();
+            startIdx++;
             continue;
         }
 
         TH1D *hist = (TH1D*)file->Get(Form("yield1Sdiff_%s_%s_pt%dto%d_%s_cosTheta%.2fto%.2f_phi%dto%d_n%lld", altSignalShapeName, altBkgShapeName, ptMin, ptMax, refFrameName, cosThetaMin, cosThetaMax, phiMin, phiMax, nPseudoExperiments));
 
-        if(i == 0){
+        if(i == startIdx){
             mergedHist = (TH1D*)hist->Clone("mergedHist");
         }
         else{
@@ -66,7 +70,7 @@ void mergeResults(const char* altSignalShapeName = "SymDSCB", const char* altBkg
     std::cout << "Histograms successfully merged into merged_output.root!" << std::endl;
 }
 
-void multipleMergeResults(Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = kFALSE, const char* signalShapeName = "SymDSCB", const char* bkgShapeName = "ExpTimesErr") {
+void multipleMergeResults(Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = kFALSE, const char* signalShapeName = "SymDSCB", const char* bkgShapeName = "ChebychevOrder3") {
 
     Int_t nCosThetaBins = 5; 
     Double_t cosThetaMin = -0.7; 
@@ -82,7 +86,7 @@ void multipleMergeResults(Int_t ptMin = 0, Int_t ptMax = 2, Bool_t isCSframe = k
 
     for (Int_t i = 0; i < nCosThetaBins; ++i) {
         for (Int_t j = 0; j < nPhiBins; ++j) {
-            mergeResults(signalShapeName, bkgShapeName, ptMin, ptMax, isCSframe, cosThetaEdges[i], cosThetaEdges[i+1], phiEdges[j], phiEdges[j+1], 100);
+            mergeResults(signalShapeName, bkgShapeName, ptMin, ptMax, isCSframe, cosThetaEdges[i], cosThetaEdges[i+1], phiEdges[j], phiEdges[j+1], 10);
         }
     }
 }
