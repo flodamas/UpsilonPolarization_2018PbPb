a4eec2b8c3f1bce79974ebc6c5d5b513741ab6b3
Florian Damas <florian.damas@cern.ch>
2025-02-26 22:04:15 +0100

    separate folder for paper figures

--- Tools/Graphs.h
+++ Tools/Graphs.h
@@ -1,2 +1,18 @@
-TGraph2DAsymmErrors* My2DGraph() {
+#include "TCanvas.h"
+
+// canvas
+TCanvas* MyCanvas(const char* name, Int_t width = 650, Int_t height = 600) {
+	auto* c = new TCanvas(name, "", width, height);
+	c->SetFillColor(0);
+	return c;
 }
+
+void SaveMyCanvas(TCanvas* canv, const char* name) {
+	canv->Update();
+	canv->RedrawAxis();
+	gSystem->mkdir("Figures/", kTRUE);
+	canv->SaveAs(Form("Figures/%s.pdf", name), "RECREATE");
+	//canv->Close();
+}
+
+// graphs
